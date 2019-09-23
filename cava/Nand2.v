4c364463ed0e4405c17690b6c2474c0b4725e424
Satnam Singh <satnam@raintown.org>
2019-09-23 12:05:17 -0700

    Separate Nand2

---
+++ cava/Nand2.v
@@ -0,0 +1,7 @@
+Require Import Cava.
+From Coq Require Import Ascii String.
+
+Definition nand2Alt i01 := Cava.Inv (And2 i01).
+
+Definition and2Alt_top := Output "o" (nand2Alt (Signal "i0", Signal "i1")).
+
