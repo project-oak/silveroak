a283e807ad1dea5d4ecf005402dc5a6ed6b74dc8
Satnam Singh <satnam@google.com>
2019-09-23 14:35:38 -0700

    Work on find inputs

--- cava/Cava2VHDL.hs
+++ cava/Cava2VHDL.hs
@@ -1,13 +1,20 @@
 module Cava2VHDL where
+
+import Data.List(nub)
+
 import Cava
 import qualified Datatypes
+
+
+findInputs :: Coq_cava -> [String]
+findInputs = nub . findInputs' 
 
 findInputs' :: Coq_cava -> [String]
 findInputs' c =
   case c of
     Inv x -> findInputs x
-    And2 (Datatypes.Coq_prod i0 i1) -> findInputs i0 ++ findInputs i1
-    Or2  (Datatypes.Coq_prod i0 i1) -> findInputs i0 ++ findInputs i1
-    Delay x -> findInpiuts x
+    And2 (Datatypes.Coq_pair i0 i1) -> findInputs i0 ++ findInputs' i1
+    Or2  (Datatypes.Coq_pair i0 i1) -> findInputs i0 ++ findInputs' i1
+    Delay x -> findInputs' x
     Signal name -> [name]
     Output _ name -> [name]
--- cava/Extract.v
+++ cava/Extract.v
@@ -3,6 +3,7 @@ Require Import Nand2.
 From Coq Require Import Extraction.
 
 Extraction Language Haskell.
-Extraction Library Cava.
+
+Recursive Extraction Library Cava.
 Extraction Library Nand2.
 (* Separate Extraction Nand2. *)
