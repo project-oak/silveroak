9e4014f02d3cc7d2a0d441862d789c12875057e4
Satnam Singh <satnam@raintown.org>
2019-09-25 09:47:25 -0700

    Use list notation from standard library

--- cava/Nand2.v
+++ cava/Nand2.v
@@ -1,10 +1,7 @@
 Require Import Cava.
 From Coq Require Import Ascii String.
 Require Import Coq.Lists.List.
-
-Notation "x :: l" := (cons x l) (at level 60, right associativity).
-Notation "[ ]" := nil.
-Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+Import ListNotations.
 
 Definition nand2Alt i01 := Cava.Inv (And2 i01).
 
