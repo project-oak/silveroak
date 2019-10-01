9a3434d8638c8b365f3ceb7aede05b71429a9429
Satnam Singh <satnam@google.com>
2019-10-01 15:18:32 -0700

    Fix nand2 testbench

--- cava/nand2_gate_tb.vhdl
+++ cava/nand2_gate_tb.vhdl
@@ -3,9 +3,11 @@ end entity nand2_gate_tb;
 
 library ieee;
 use ieee.std_logic_1164.all;
+use work.nand2_gate_package.all;
 architecture simulation of nand2_gate_tb is
   signal i0, i1, o : std_ulogic;
 begin
+  nand2_inst : nand2_gate port map (i0 => i0, i1 => i1, o => o);
   sim : process is
     begin
       i0 <= '0'; i1 <= '0'; wait for 10 ns;
