fac56e355ed0be63fdf22aa81d2475c0e84ce3fa
Satnam Singh <satnam@raintown.org>
2019-10-21 22:51:58 -0700

    Alternative one bit adder

--- cava/AltOneBitAdder.v
+++ cava/AltOneBitAdder.v
@@ -22,23 +22,50 @@ Require Import Coq.Program.Basics.
 Local Open Scope program_scope.
 
 
-Definition reorg1 (cinab : NetExpr (Tuple2 Bit (Tuple2 Bit Bit))) :
-                  NetExpr (Tuple2 (Tuple2 Bit Bit) (Tuple2 Bit Bit))
+Definition reorg1 {cin a b : signal}
+                  (cinab : NetExpr (Tuple2 cin (Tuple2 a b))) :
+                  NetExpr (Tuple2 (Tuple2 cin a) (Tuple2 a b))
   := match cinab with
-     | NetPair cin (NetPair a b) => NetPair (NetPair cin a) (NetPair a b)
+       NetPair cin ab => match ab with
+                           NetPair a b => NetPair (NetPair cin a) (NetPair a b)
+                         end
      end.
 
-Definition oneBitAdder
-  := Rewire reorg1 ⟼ second Xor2.
+
+Definition reorg2 (cinaps : NetExpr (Tuple2 (Tuple2 Bit Bit) Bit)) :
+                  NetExpr (Tuple2 (Tuple2 Bit Bit) (Tuple2 Bit (Tuple2 Bit Bit)))
+  := match cinaps with
+       @NetPair (Tuple2 Bit Bit) Bit cina ps
+          => match cina with
+                @NetPair Bit Bit cin a => NetPair (NetPair cin ps) (NetPair ps (NetPair a cin))
+             end
+     end.
+
+Definition oneBitAdder : cava (Tuple2 Bit (Tuple2 Bit Bit)) (Tuple2 Bit Bit)
+  := Rewire reorg1 ⟼ second Xor2 ⟼ Rewire reorg2 ⟼ (Xor2 ‖ Muxcy).
 
 
+Definition oneBitAdder_top
+  := (Input "cin" ‖ (Input "a" ‖ Input "b")) ⟼
+     oneBitAdder ⟼
+     ((Output "sum") ‖ (Output "cout")).
 
 Definition forkBit (x : NetExpr Bit) : NetExpr (Tuple2 Bit Bit)
   := match x with
      | Net n => NetPair (Net n) (Net n)
      end.
 
- Definition swapBit (x : NetExpr (Tuple2 Bit Bit)) : NetExpr (Tuple2 Bit Bit)
+Definition swapBit (x : NetExpr (Tuple2 Bit Bit)) : NetExpr (Tuple2 Bit Bit)
   := match x with
-     | NetPair a b => NetPair b a
-     end.    
+     | @NetPair Bit Bit x y => NetPair y x
+     end.
+
+
+Definition tupleLeft {a b c : signal} (x:NetExpr (Tuple2 a (Tuple2 b c))) :
+                                      NetExpr (Tuple2 (Tuple2 a b) c)
+   := match x with
+      | NetPair p qr => match qr with
+                          NetPair q r => NetPair (NetPair p q) r
+                        end
+      end.
+
