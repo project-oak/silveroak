f878db8eab1df451ceec8e5661cb01fa2bc1755c
Satnam Singh <satnam@google.com>
2019-11-15 17:11:53 -0800

    Recase Cava with state monad library from John Wiegley

--- cava/CavaMonad.v
+++ cava/CavaMonad.v
@@ -1,32 +1,86 @@
-Class Monad (m : Type -> Type) : Type := { 
-    ret : forall {t}, t -> m t ;
-    bind : forall {t u}, m t -> (t -> m u) -> m u
+Require Import Program.Basics.
+From Coq Require Import Bool.Bool.
+From Coq Require Import Ascii String.
+From Coq Require Import Lists.List.
+Import ListNotations.
+
+Require Import Hask.Control.Monad.
+Require Import Hask.Control.Monad.State.
+
+Local Open Scope list_scope.
+Local Open Scope monad_scope.
+
+Definition increment : State nat nat
+  := v <- get ;
+     put (v+1) ;;
+     return_ (v+1).
+
+Definition comp1 : nat * nat := increment 0.
+Definition run1 : nat := snd comp1.
+
+Check run1.
+Eval cbv in run1.
+
+
+Class Cava m t  := {
+  inv : t -> m t;
+  and2 : t * t -> m t;
 }.
 
-Class MonadState (T : Type) (m : Type -> Type) : Type := {
-    get : m T ;
-    put : T -> m unit
+Record Instance : Type := mkInstance {
+  inst_name : string;
+  inst_args : list nat;
 }.
 
-Notation "x <- c1 ;; c2" := (bind c1 (fun x => c2))
-                             (right associativity, at level 84, c1 at next level).
-
-Notation "c1 ;; c2" := (bind c1 (fun _ => c2)) (at level 100, right associativity).
-
-Definition state := nat.
-Definition state_comp(A:Type) := state -> (state*A).
-
-Instance StateMonad : Monad state_comp := {
-  ret := fun {A:Type} (x:A) => (fun (s:state) => (s,x)) ;
-  bind := fun {A B:Type} (c : state_comp A) (f: A -> state_comp B) =>
-            fun (s:state) =>
-              let (s',v) := c s in
-              f v s'
+Record CavaState : Type := mkCavaState {
+  netNumber : nat;
+  instances : list Instance;
 }.
 
-Definition read : state_comp nat :=
-  fun s => (s, s).
 
-Definition write (n:nat) : state_comp nat :=
-  fun s => (n, n).
+Definition invNet (i:nat) : State CavaState nat :=
+  cs <- get;
+  match cs with
+  | mkCavaState o insts => put (mkCavaState (o+1) (cons (mkInstance "inv" [i; o]) insts)) ;;
+                           return_ o
+  end. 
 
+Definition and2Net (i0i1 : nat * nat) : State CavaState nat :=
+  cs <- get;
+  match cs with
+  | mkCavaState o insts => put (mkCavaState (o+1) (cons (mkInstance "and2" [fst i0i1; snd i0i1; o]) insts)) ;;
+                           return_ o
+  end.
+
+Instance CavaNet : Cava (State CavaState) nat :=
+  { inv := invNet;
+    and2 := and2Net;
+}.
+
+Definition nand2 := and2 >=> inv.
+
+Definition invBool (i : bool) : State unit bool :=
+  return_ (negb i).
+
+Definition and2Bool (i0i1 : bool * bool) : State unit bool :=
+  let (i0, i1) := i0i1 in
+  return_ (i0 && i1).
+
+Instance CavaBool : Cava (State unit) bool :=
+  { inv := invBool;
+    and2 := and2Bool;
+}.
+
+Eval simpl in fst ((inv false) tt). 
+Eval simpl in fst ((inv true) tt).
+
+Eval simpl in fst ((and2 (false, false)) tt).
+Eval simpl in fst ((and2 (true, true)) tt).
+
+(*
+Eval simpl in fst ((nand2 (false, false)) tt).
+Eval simpl in fst ((nand2 (true, true)) tt).
+*)
+
+Eval cbv in ((inv 0) (mkCavaState 1 [])).
+Eval cbv in ((nand2 0) (mkCavaState 1 [])).
