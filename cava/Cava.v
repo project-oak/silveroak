268814b664450f7cb997a998d8c4524bdc46e866
Satnam Singh <satnam@google.com>
2019-12-18 15:37:06 -0800

    Improve cleanup

--- cava/Cava.v
+++ cava/Cava.v
@@ -1,3 +1,24 @@
+(****************************************************************************)
+(* Copyright 2019 The Project Oak Authors                                   *)
+(*                                                                          *)
+(* Licensed under the Apache License, Version 2.0 (the "License")           *)
+(* you may not use this file except in compliance with the License.         *)
+(* You may obtain a copy of the License at                                  *)
+(*                                                                          *)
+(*     http://www.apache.org/licenses/LICENSE-2.0                           *)
+(*                                                                          *)
+(* Unless required by applicable law or agreed to in writing, software      *)
+(* distributed under the License is distributed on an "AS IS" BASIS,        *)
+(* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *)
+(* See the License for the specific language governing permissions and      *)
+(* limitations under the License.                                           *)
+(****************************************************************************)
+
+(* A codification of the Lava embedded DSL develope for Haskell into
+   Coq for the specification, implementaiton and formal verification of circuits.
+   Experimental work, very much in flux, as Satnam learns Coq!
+*)
+
 Require Import Program.Basics.
 From Coq Require Import Bool.Bool.
 From Coq Require Import Ascii String.
@@ -10,11 +31,15 @@ Require Import Hask.Control.Monad.State.
 Local Open Scope list_scope.
 Local Open Scope monad_scope.
 
-Class Cava m t  := {
+Set Printing Implicit.
+Set Printing All.
+
+Class Cava m t `{Monad m} := {
   inv : t -> m t;
   and2 : t * t -> m t;
 }.
 
+Definition nand2 {m t} `{Monad m} `{Cava m t} := and2 >=> inv.
 
 Record Instance : Type := mkInstance {
   inst_name : string;
@@ -46,7 +71,6 @@ Instance CavaNet : Cava (State CavaState) nat :=
     and2 := and2Net;
 }.
 
-Definition nand2 := and2 >=> inv.
 
 Eval cbv in ((nand2 (0, 1)) (mkCavaState 2 [])).
 
@@ -69,12 +93,11 @@ Eval simpl in fst ((inv true) tt).
 Eval simpl in fst ((and2 (false, false)) tt).
 Eval simpl in fst ((and2 (true, true)) tt).
 
-(*
+
 Eval simpl in fst ((nand2 (false, false)) tt).
 Eval simpl in fst ((nand2 (true, true)) tt).
-*)
+
 
 Eval cbv in ((inv 0) (mkCavaState 1 [])).
-(*
-Eval cbv in ((nand2 0) (mkCavaState 1 [])).
-*)
+
+
