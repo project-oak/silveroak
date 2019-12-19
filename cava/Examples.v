96afc0a49ddc0d2bf92bf04341b90cb2d474bd87
Satnam Singh <satnam@google.com>
2019-12-18 16:08:36 -0800

    Separate out examples from core Cava definitions

--- cava/Examples.v
+++ cava/Examples.v
@@ -28,61 +28,26 @@ Import ListNotations.
 Require Import Hask.Control.Monad.
 Require Import Hask.Control.Monad.State.
 
+Require Import Cava.
+
 Local Open Scope list_scope.
 Local Open Scope monad_scope.
 
-Set Printing Implicit.
-Set Printing All.
+(* Experiments with the pre-defined gates. *)
 
-Class Cava m t `{Monad m} := {
-  inv : t -> m t;
-  and2 : t * t -> m t;
-}.
+Eval simpl in fst ((inv false) tt). 
+Eval simpl in fst ((inv true) tt).
 
-Record Instance : Type := mkInstance {
-  inst_name : string;
-  inst_args : list nat;
-}.
+Eval simpl in fst ((and2 (false, false)) tt).
+Eval simpl in fst ((and2 (true, true)) tt).
 
-Record CavaState : Type := mkCavaState {
-  netNumber : nat;
-  instances : list Instance;
-}.
+Eval cbv in ((inv 0) (mkCavaState 1 [])).
 
+(* NAND gate examples. *)
 
-Definition invNet (i:nat) : State CavaState nat :=
-  cs <- get;
-  match cs with
-  | mkCavaState o insts => put (mkCavaState (o+1) (cons (mkInstance "inv" [i; o]) insts)) ;;
-                           return_ o
-  end. 
+Definition nand2 {m t} `{Monad m} `{Cava m t} := and2 >=> inv.
 
-Definition and2Net (i0i1 : nat * nat) : State CavaState nat :=
-  cs <- get;
-  match cs with
-  | mkCavaState o insts => put (mkCavaState (o+1) (cons (mkInstance "and2" [fst i0i1; snd i0i1; o]) insts)) ;;
-                           return_ o
-  end.
+Eval cbv in ((nand2 (0, 1)) (mkCavaState 2 [])).
 
-Instance CavaNet : Cava (State CavaState) nat :=
-  { inv := invNet;
-    and2 := and2Net;
-}.
-
-Definition invBool (i : bool) : State unit bool :=
-  return_ (negb i).
-
-Definition and2Bool (i0i1 : bool * bool) : State unit bool :=
-  let (i0, i1) := i0i1 in
-  return_ (i0 && i1).
-
-Instance CavaBool : Cava (State unit) bool :=
-  { inv := invBool;
-    and2 := and2Bool;
-}.
-
-
-
-
-
-
+Eval simpl in fst ((nand2 (false, false)) tt).
+Eval simpl in fst ((nand2 (true, true)) tt).
