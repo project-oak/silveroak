c6469aad26fc2f235886fb34ef1ef8604ded4971
Satnam Singh <satnam@google.com>
2020-01-14 15:02:17 -0800

    Create a separate directory for Cava circuit examples

---
+++ cava-examples/NandGate.v
@@ -0,0 +1,56 @@
+(****************************************************************************)
+(* Copyright 2020 The Project Oak Authors                                   *)
+(*                                                                          *)
+(* Licensed under the Apache License, Version 2.0 (the "License")           *)
+(* you may not use this file except in compliance with the License.         *)
+(* You may obtain a copy of the License at                                  *)
+(*                                                                          *)
+(*     http://www.apache.org/licenses/LICENSE-2.0                           *)
+(*                                                                          *)
+(* Unless required by applicable law or agreed to in writing, software      *)
+(* distributed under the License is distributed on an "AS IS" BASIS,        *)
+(* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *)
+(* See the License for the specific language governing permissions and      *)
+(* limitations under the License.                                           *)
+(****************************************************************************)
+
+(* A codification of the Lava embedded DSL develope for Haskell into
+   Coq for the specification, implementaiton and formal verification of
+   circuits. Experimental work, very much in flux, as Satnam learns Coq!
+*)
+
+Require Import Program.Basics.
+From Coq Require Import Bool.Bool.
+From Coq Require Import Ascii String.
+From Coq Require Import Lists.List.
+From Coq Require Import ZArith.
+Import ListNotations.
+
+Require Import Hask.Control.Monad.
+Require Import Hask.Control.Monad.State.
+
+Require Import Cava.
+
+Local Open Scope list_scope.
+Local Open Scope monad_scope.
+
+(* NAND gate example. Fist, let's define an overloaded NAND gate
+   description. *)
+
+Definition nand2 {m t} `{Cava m t} := and_gate >=> not_gate.
+
+Definition nand2Top {m t} `{CavaTop m t} :=
+  setModuleName "nand2" ;;
+  a <- input "a" ;
+  b <- input "b" ;
+  c <- nand2 [a; b] ;
+  output "c" c.
+
+Definition nand2Netlist := snd (nand2Top initState).
+
+(* A proof that the NAND gate implementation is correct. *)
+Lemma nand2_behaviour : forall (a : bool) (b : bool),
+                        (fst (nand2 [a; b] tt)) = negb (a && b).
+Proof.
+  auto.
+Qed.
---
+++ cava-examples/nand2_tb.sv
@@ -0,0 +1,63 @@
+//
+// Copyright 2019 The Project Oak Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+module nand2_test;
+  
+  timeunit 1ns; timeprecision 1ns;
+
+  logic a, b, c;
+  logic rst;
+  logic clk;
+  
+  nand2 nand2_dut  (.*);
+
+  initial begin
+    clk <= 0;
+    forever #5 clk = ~clk;
+  end
+ 
+  initial $monitor($time, " a = %b, b = %b, c = %b", a, b, c);
+
+  initial begin
+    assign a = 1'b0;
+    assign b = 1'b0;
+    assign rst = 1'b1;
+    @(posedge clk);
+    @(posedge clk);
+    assign rst = 1'b0;
+    @(posedge clk);
+    assign a = 1'b1;
+    assign b = 1'b0;
+    @(posedge clk);
+    assign a = 1'b0;
+    assign b = 1'b1;
+    @(posedge clk);
+    assign a = 1'b1;
+    assign b = 1'b1;
+    @(posedge clk);
+    assign a = 1'b0;
+    assign b = 1'b0;
+    @(posedge clk);
+    @(negedge clk) $finish;
+  end
+  
+  initial
+  begin
+    $dumpfile("nand2.vcd");
+    $dumpvars;
+  end
+
+endmodule
