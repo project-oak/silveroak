2ed448343b5cc0f0c60d890adc89cf7a464c8851
Satnam Singh <satnam@raintown.org>
2020-01-14 21:19:31 -0800

    Add the full adder design

---
+++ cava-examples/FullAdder.v
@@ -0,0 +1,102 @@
+(****************************************************************************)
+(* Copyright 2020 The Project Oak Authors                                   *)
+(*                                                                          *)
+(* Licensed under the Apache License, Version 2.0 (the "License")           *)
+(* you may not use this file except in compliance with the License.         *)
+(* You may obtain a copy of the License at                                  *)
+(*                                                                          *)
+(*     http://www.apache.org/licenses/LICENSE-2.0                           *)
+(*                                                                          *)
+(* Unless required by applicable law or agreed to in writing, software      *)
+(* distributed under the License is distributed on an "AS IS" BASIS,        *)
+(* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *)
+(* See the License for the specific language governing permissions and      *)
+(* limitations under the License.                                           *)
+(****************************************************************************)
+
+(* Cava implementations of the gates described in the Nand Game website
+   which describes how to build circuit components using only NAND gates
+   and registers. http://nandgame.com/
+*)
+
+From Coq Require Import Bool.Bool.
+From Coq Require Import Ascii String.
+From Coq Require Import Lists.List.
+Import ListNotations.
+
+Require Import Hask.Control.Monad.
+
+Require Import Cava.
+
+Local Open Scope list_scope.
+Local Open Scope monad_scope.
+
+
+(******************************************************************************)
+(* Build a half-adder                                                         *)
+(******************************************************************************)
+
+Definition halfAdder {m t} `{Cava m t} a b :=
+  partial_sum <- xor_gate [a; b] ;
+  carry <- and_gate [a; b] ;
+  return_ (partial_sum, carry).
+
+Definition halfAdderTop {m t} `{CavaTop m t} :=
+  setModuleName "halfadder" ;;
+  a <- input "a" ;
+  b <- input "b" ;
+  ps_c <- halfAdder a b ;
+  output "partial_sum" (fst ps_c) ;;
+  output "carry" (snd ps_c).
+
+Definition halfAdderNetlist := makeNetlist halfAdderTop.
+
+(* A proof that the the half-adder is correct. *)
+Lemma halfAdder_behaviour : forall (a : bool) (b : bool),
+                            combinational (halfAdder a b) = (xorb a b, a && b).
+
+Proof.
+  intros.
+  unfold combinational.
+  unfold fst.
+  simpl.
+  case a, b.
+  all : reflexivity.
+Qed.
+
+   
+(******************************************************************************)
+(* Build a full-adder                                                         *)
+(******************************************************************************)
+
+Definition fullAdder {m t} `{Cava m t} a b cin :=
+  abl_abh <- halfAdder a b ;
+  abcl_abch <- halfAdder (fst abl_abh) cin ;
+  cout <- or_gate [snd abl_abh; snd abcl_abch] ;
+  return_ (fst abcl_abch, cout).
+
+Definition fullAdderTop {m t} `{CavaTop m t} :=
+  setModuleName "fulladder" ;;
+  a <- input "a" ;
+  b <- input "b" ;
+  cin <- input "cin" ;
+  sum_cout <- fullAdder a b cin ;
+  output "sum" (fst sum_cout) ;;
+  output "carry" (snd sum_cout).
+
+
+Definition fullAdderNetlist := makeNetlist fullAdderTop.
+
+(* A proof that the the full-adder is correct. *)
+Lemma fullAdder_behaviour : forall (a : bool) (b : bool) (cin : bool),
+                            combinational (fullAdder a b cin)
+                              = (xorb cin (xorb a b),
+                                 (a && b) || (b && cin) || (a && cin)).
+Proof.
+  intros.
+  unfold combinational.
+  unfold fst.
+  simpl.
+  case a, b, cin.
+  all : reflexivity.
+Qed.
