c2fb70e2ca0460c816c37ea849376cd4ceedb849
Satnam Singh <satnam@google.com>
2020-01-15 15:58:11 -0800

    Coq evaluation of full adder example

--- cava-examples/UnsignedAdder.v
+++ cava-examples/UnsignedAdder.v
@@ -28,20 +28,44 @@ Require Import FullAdder.
 Local Open Scope list_scope.
 Local Open Scope monad_scope.
 
-(*
-
-Need to rework to make decreasing argument clear.
 
 Fixpoint unsignedAdder {m bit} `{Cava m bit}
-                       (n:N) (a : list bit) (b : list bit) (cin : bit)
+                       (n : nat) (a : list bit) (b : list bit) (cin : bit)
   : m (list bit)
   :=
   match n with
-  | N0 => return_ []
-  | s  => sum_cout <- fullAdderFC (hd cin a) (hd cin b) cin ;
-         r <- unsignedAdder (n - 1) (tl a) (tl b) (snd sum_cout) ;
-         return_ (cons (fst sum_cout) r)
+  | 0 => return_ [cin]
+  | S n  => sum_cout <- fullAdderFC (hd cin a) (hd cin b) cin ;
+            let sum := fst sum_cout in
+            let cout := snd sum_cout in 
+            r <- unsignedAdder n (tl a) (tl b) cout ;
+            return_ (cons sum r)
   end.
 
+Definition adder8 := unsignedAdder 8.
 
-*)
+Definition nat2bool (n : nat) : bool :=
+  match n with
+  | 0 => false
+  | _ => true
+  end.
+
+Definition toVec := map nat2bool.
+
+Definition bool2nat (b : bool) : nat :=
+  match b with
+  | false => 0
+  | true => 1
+  end.
+
+Definition fromVec := map bool2nat.
+
+Definition v1 := toVec [0;1;0;0;0;0;0;0].
+Definition v2 := toVec [1;0;0;0;0;0;0;0].
+
+Compute (fromVec (combinational (adder8 v1 v2 false))).
+
+Definition v3 := toVec [1;1;1;1;1;1;1;1].
+Definition v4 := toVec [1;0;0;0;0;0;0;0].
+
+Compute (fromVec (combinational (adder8 v3 v4 false))).
