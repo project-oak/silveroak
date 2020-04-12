d00e2a8ba809f12e07582a9f7e3df9160283df30
Satnam Singh <satnam@google.com>
2020-04-12 02:19:51 -0700

    Convert adder_tree8_4 test bench to use verilator. (#61)

--- cava/monad-examples/testbench/adder_tree4_8_tb.sv
+++ cava/monad-examples/testbench/adder_tree4_8_tb.sv
@@ -14,36 +14,42 @@
 // limitations under the License.
 //
 
-module adder_tree4_8_test;
+module adder_tree4_8_tb(
+   input logic clk
+);
   
   timeunit 1ns; timeprecision 1ns;
 
   logic [7:0] a, b, c, d;
   logic [7:0] sum;
-  
-  adder_tree4_8 adder_tree4_8_inst  (.*);
- 
-  initial $monitor($time, " a = %0d, b = %0d,  c = %0d, d = %0d, sum = %0d", 
-                   a, b, c, d, sum);
 
-  initial begin
-    assign a   = 8'd4;
-    assign b   = 8'd5;
-    assign c   = 8'd11;
-    assign d   = 8'd9;
-    #10
-    assign a   = 8'd15;
-    assign b   = 8'd3;
-    assign c   = 8'd200;
-    assign d   = 8'd7;
-    #10
-    $finish;
-  end
-  
-  initial
-  begin
-    $dumpfile("adder_tree4_8.vcd");
-    $dumpvars;
+  adder_tree4_8 adder_tree4_8_inst  (.*);
+
+  logic [7:0] test_vectors[2][4]
+    = '{ '{ 8'd4,  8'd5, 8'd11,  8'd9 },
+         '{ 8'd15, 8'd3, 8'd200, 8'd7 }
+       };
+
+  logic[7:0] expected_output[2] = '{ 8'd29, 8'd225 };     
+
+  int unsigned i = 0;
+
+  assign a = test_vectors[i][0];
+  assign b = test_vectors[i][1];
+  assign c = test_vectors[i][2];
+  assign d = test_vectors[i][3]; 
+
+  always @(posedge clk) begin
+    $display ($time, " i = %0d, a = %0d, b = %0d, c = %0d, d = %0d, sum = %0d", i, a, b, c, d, sum);
+    if (sum != expected_output[i]) begin
+      $error("Expected %0d but got %0d", expected_output[i], sum);
+    end;
+    if (i == 1) begin
+      $display ("PASSED");
+      $finish;
+    end else begin
+      i <= i + 1 ;
+    end;
   end
 
 endmodule
