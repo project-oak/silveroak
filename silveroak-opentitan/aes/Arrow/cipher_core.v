591b1cef2172b9300d6b5f139d3f64d8c89224c4
Ben Blaxill <benblaxill@google.com>
2020-11-12 04:42:12 +0000

    update with new notation

--- silveroak-opentitan/aes/Arrow/cipher_core.v
+++ silveroak-opentitan/aes/Arrow/cipher_core.v
@@ -21,7 +21,127 @@ From Aes Require Import pkg mix_columns sbox sub_bytes shift_rows cipher_round k
 
 Import VectorNotations.
 Import KappaNotation.
-Open Scope kind_scope.
+Open Scope kappa_scope.
+
+Definition key_expand_and_round (sbox_impl: SboxImpl) :=
+  <[ fun "key_expand_and_round"
+    (op_i: _ Bit)
+    (round: _ (Vector Bit 4))
+    (data_i: _ (Vector (Vector (Vector Bit 8) 4) 4))
+    (key: _ (Vector (Vector (Vector Bit 8) 4) 8))
+    (step_i: _ Bit)
+    (clear_i: _ Bit)
+    : << Vector (Vector (Vector Bit 8) 4) 4 (* data *)
+      ,  Vector (Vector (Vector Bit 8) 4) 8 (* key *)
+      >> =>
+
+    let key_words = !aes_transpose
+      (if op_i == !CIPH_FWD
+      then key[:7:4]
+      else key[:3:0])
+      in
+    let round_key =
+      if op_i == !CIPH_FWD
+      then key_words
+      else !aes_mix_columns !CIPH_INV key_words in
+
+    let data_o =
+      !(cipher_round_combined sbox_impl) op_i data_i round_key (round == #0) (round == #14) in
+
+    let new_key = !(aes_key_expand sbox_impl) op_i step_i clear_i round key in
+    (data_o, new_key)
+  ]>.
+
+Definition aes_cipher_core_mealy
+  (sbox_impl: SboxImpl) :=
+  <[ fun "aes_cipher_core_mealy"
+
+    (op_i: _ Bit)
+    (data_i: _ (Vector (Vector (Vector Bit 8) 4) 4))
+    (data_valid: _ Bit)
+    (key_i: _ (Vector (Vector (Vector Bit 8) 4) 8))
+    (key_valid: _ Bit)
+
+    (data_o_ready: _ Bit)
+
+    (op_s: _ Bit)
+    (data_s: _ (Vector (Vector (Vector Bit 8) 4) 4))
+    (key_s: _ (Vector (Vector (Vector Bit 8) 4) 8))
+    (current_round: _ (Vector Bit 4))
+    (* (done: _ Bit) *)
+
+    : << Bit (* Accept data *)
+       , Bit (* Accept key *)
+
+       , Bit (* op_i *)
+       , Vector (Vector (Vector Bit 8) 4) 4 (*data_s*)
+       , Vector (Vector (Vector Bit 8) 4) 8 (*key_s *)
+       , Vector Bit 4 (* next round *)
+       , Bit (* valid output *)
+
+       >> =>
+
+    let step = false' in
+    let clear = false' in
+    let '(data_n, key_n) = !(key_expand_and_round sbox_impl) op_i current_round data_s key_s step clear in
+    let round_inc = current_round + #1 in
+
+    let done = current_round == #14 || current_round == #15 in
+    let reset = current_round == #15 && data_o_ready in
+    let next_round =
+      if done then
+        if reset then #0
+        else #15
+      else round_inc in
+
+
+    ( _, _, _,
+    _,
+    _,
+    next_round,
+    (current_round == #15))
+
+      ]>.
+
+Definition aes_cipher_core
+  (sbox_impl: SboxImpl)
+      :=
+  <[ fun "aes_cipher_core"
+    (op_i: _ Bit)
+    (data_i: _ (Vector (Vector (Vector Bit 8) 4) 4))
+    (data_valid: _ Bit)
+    (key: _ (Vector (Vector (Vector Bit 8) 4) 8))
+    (key_valid: _ Bit) :
+      << (Vector (Vector (Vector Bit 8) 4) 4)
+       , Bit >>
+      =>
+
+    letrec _ = aes_cipher_core_mealy
+
+
+    (* OpenTitan runs the AES cipher core forwards to generate the decryption
+    keys. Since this is an unrolled variant we just duplicate the core logic *)
+    let rcon_initial = #1 in
+    let forward_keys = concat key_i[:7:4] key_i[:3:0] in
+
+    let '(_, fwd_end_state) =
+      !(foldl (n:=14) (key_expand_and_round sbox_impl))
+      (!CIPH_FWD, rcon_initial, data_i, forward_keys) !(seq 0) in
+    let '(data_i', key') = snd fwd_end_state in
+    let forward_o = !(final_cipher_round sbox_impl) !CIPH_FWD data_i' (!aes_transpose key'[:7:4]) in
+
+    let backward_keys = concat key'[:7:4] key'[:3:0] in
+
+    let '(_, bkw_end_state) =
+      !(foldl (n:=14) (key_expand_and_round sbox_impl))
+      (!CIPH_INV, #64, data_i, backward_keys) !(seq 0) in
+    let '(rev_data_i', rev_key) = snd bkw_end_state in
+    let backward_o = !(final_cipher_round sbox_impl) !CIPH_INV rev_data_i' (!aes_transpose rev_key[:3:0]) in
+
+    if op_i == !CIPH_FWD
+    then forward_o
+    else backward_o
+    ]>.
 
 Definition aes_cipher_core
   (sbox_impl: SboxImpl)
