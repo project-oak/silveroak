be204ad068824fd1b34f9f572aef5c4cd7821d99
Satnam Singh <satnam@google.com>
2020-11-17 00:52:09 -0800

    Port of XilinxAdder

--- cava/Cava/Monad/XilinxAdder.v
+++ cava/Cava/Monad/XilinxAdder.v
@@ -33,66 +33,78 @@ Local Open Scope string_scope.
 Local Open Scope list_scope.
 Local Open Scope type_scope.
 
-(******************************************************************************)
-(* Build a full-adder with explicit use of Xilinx FPGA fast carry logic       *)
-(******************************************************************************)
+Section WithCava.
+  Context {signal} {m : Cava signal}.
+  Context {monad: Monad cava}.
 
-Definition xilinxFullAdder {m bit} `{Cava m bit}
-          (cin: bit)
-          (ab: bit * bit)
-  : m (bit * bit)%type :=
-  let (a, b) := ab in
-  part_sum <- xor2 (a, b) ;;
-  sum <- xorcy (part_sum, cin) ;;
-  cout <- muxcy part_sum cin a  ;;
-  ret (sum, cout).
+  (****************************************************************************)
+  (* Build a full-adder with explicit use of Xilinx FPGA fast carry logic     *)
+  (****************************************************************************)
 
-(* A proof that the the full-adder is correct. *)
-Lemma xilinxFullAdder_behaviour :
-  forall (a : bool) (b : bool) (cin : bool),
-         combinational (xilinxFullAdder cin (a, b))
-         = (xorb cin (xorb a b),
-           (a && b) || (b && cin) || (a && cin)).
-Proof.
-  intros.
-  unfold combinational.
-  unfold fst.
-  simpl.
-  case a, b, cin.
-  all : reflexivity.
-Qed.
+  Definition xilinxFullAdder
+            (cin: signal Bit)
+            (ab: signal Bit * signal Bit)
+    : cava (signal Bit * signal Bit) :=
+    let (a, b) := ab in
+    part_sum <- xor2 (a, b) ;;
+    sum <- xorcy (part_sum, cin) ;;
+    cout <- muxcy part_sum cin a  ;;
+    ret (sum, cout).
 
-(******************************************************************************)
-(* An unsigned adder built using the fast carry full-adder.                   *)
-(******************************************************************************)
+  (******************************************************************************)
+  (* An unsigned adder built using the fast carry full-adder.                   *)
+  (******************************************************************************)
 
-Definition xilinxAdderWithCarry {m bit} `{Cava m bit} {n: nat}
-            (cinab : bit * (Vector.t bit n * Vector.t bit n))
-           : m (Vector.t bit n * bit)
-  := let '(cin, (a, b)) := cinab in
-     '(sum, cout) <- colV n xilinxFullAdder cin (vcombine a b) ;;
-     ret (sum, cout).
+  Definition xilinxAdderWithCarry {n: nat}
+              (cinab : signal Bit * (signal (Vec Bit n) * signal (Vec Bit n)))
+            : cava (signal (Vec Bit n) * signal Bit)
+    := let '(cin, (a, b)) := cinab in
+      let a0 := peel a in
+      let b0 := peel b in
+      '(sum, cout) <- colV n xilinxFullAdder cin (vcombine a0 b0) ;;
+      ret (unpeel sum, cout).
 
-(* A quick sanity check of the Xilinx adder with carry in and out *)
-Example xilinx_add_17_52:
-  combinational (xilinxAdderWithCarry
-                (false, (N2Bv_sized 8 17, N2Bv_sized 8 52))) =
-                (N2Bv_sized 8 69, false).
-Proof. reflexivity. Qed.
+  (******************************************************************************)
+  (* An unsigned adder with no bit-growth and no carry in                       *)
+  (******************************************************************************)
 
-(******************************************************************************)
-(* An unsigned adder with no bit-growth and no carry in                       *)
-(******************************************************************************)
+  Definition xilinxAdder {n: nat}
+              (a b: signal (Vec Bit n))
+              : cava (signal (Vec Bit n)) :=
+    z <- zero ;;
+    '(sum, carry) <- xilinxAdderWithCarry (z, (a, b)) ;;
+    ret sum.
 
-Definition xilinxAdder {m bit} `{Cava m bit} {n: nat}
-            (a: Vector.t bit n) (b: Vector.t bit n)
-           : m (Vector.t bit n) :=
-  z <- zero ;;
-  '(sum, carry) <- xilinxAdderWithCarry (z, (a, b)) ;;
-  ret sum.
+End WithCava.
 
-(* A quick sanity check of the Xilinx adder with no bit-growth *)
-Example xilinx_no_growth_add_17_52:
-  combinational (xilinxAdder (N2Bv_sized 8 17) (N2Bv_sized 8 52)) =
-                 N2Bv_sized 8 69.
-Proof. reflexivity. Qed.
+Section WithCombinational.
+
+  (* A quick sanity check of the Xilinx adder with carry in and out *)
+  Example xilinx_add_17_52:
+    combinational (xilinxAdderWithCarry
+                  (false, (N2Bv_sized 8 17, N2Bv_sized 8 52))) =
+                  (N2Bv_sized 8 69, false).
+  Proof. reflexivity. Qed.
+
+  (* A quick sanity check of the Xilinx adder with no bit-growth *)
+  Example xilinx_no_growth_add_17_52:
+    combinational (xilinxAdder (N2Bv_sized 8 17) (N2Bv_sized 8 52)) =
+                  N2Bv_sized 8 69.
+  Proof. reflexivity. Qed.
+
+  (* A proof that the the full-adder is correct. *)
+  Lemma xilinxFullAdder_behaviour :
+    forall (a : bool) (b : bool) (cin : bool),
+          combinational (xilinxFullAdder cin (a, b))
+          = (xorb cin (xorb a b),
+            (a && b) || (b && cin) || (a && cin)).
+  Proof.
+    intros.
+    unfold combinational.
+    unfold fst.
+    simpl.
+    case a, b, cin.
+    all : reflexivity.
+  Qed.
+
+End WithCombinational.  
