5eb794de0445b9a8c88e35d99488957322fbca7e
jadephilipoom <jadep@google.com>
2020-11-17 17:57:21 +0000

    Inverse cipher equivalence (arrow-based) (#343)

---
+++ silveroak-opentitan/aes/Arrow/CipherEquivalenceCommon.v
@@ -0,0 +1,93 @@
+(****************************************************************************)
+(* Copyright 2020 The Project Oak Authors                                   *)
+(*                                                                          *)
+(* Licensed under the Apache License, Version 2.0 (the "License")           *)
+(* you may not use this file except in compliance with the License.         *)
+(* You may obtain a copy of the License at                                  *)
+(*                                                                          *)
+(*     http://www.apache.org/licenses/LICENSE-2.0                           *)
+(*                                                                          *)
+(* Unless required by applicable law or agreed to in writing, software      *)
+(* distributed under the License is distributed on an "AS IS" BASIS,        *)
+(* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *)
+(* See the License for the specific language governing permissions and      *)
+(* limitations under the License.                                           *)
+(****************************************************************************)
+
+Require Import Coq.NArith.NArith.
+Require Import Coq.Vectors.Vector.
+Require Import Cava.Arrow.ArrowKind.
+Require Import Cava.VectorUtils.
+Import VectorNotations.
+
+Module Notations.
+  Notation nat_to_bitvec size n := (Ndigits.N2Bv_sized size (N.of_nat n)).
+  Notation nat_to_byte n := (nat_to_bitvec 8 n).
+  Notation byte := (Vector.t bool 8) (only parsing).
+  Notation state := (Vector.t (Vector.t byte 4) 4) (only parsing).
+  Notation key := (Vector.t (Vector.t byte 4) 4) (only parsing).
+  Notation rconst := byte (only parsing).
+  Notation keypair := (Vector.t (Vector.t byte 4) 8) (only parsing).
+End Notations.
+Import Notations.
+
+(* aes_key_expand operates on a pair of keys; this is a sliding window of
+   (previous key ++ current key). The following definitions project out
+   keys from the pair. *)
+Definition fstkey : keypair -> key :=
+  @slice_by_position
+    (t (t bool 8) 4) 8 3 0 (kind_default (Vector (Vector Bit 8) 4)).
+Definition sndkey : keypair -> key :=
+  @slice_by_position
+    (t (t bool 8) 4) 8 7 4 (kind_default (Vector (Vector Bit 8) 4)).
+
+Definition swap_keys (k : keypair) := sndkey k ++ fstkey k.
+
+Local Ltac constant_vector_simpl vec :=
+  lazymatch type of vec with
+  | t _ (S ?n) =>
+    let v' := fresh "v" in
+    rewrite (eta vec); set (v':=tl vec);
+    cbv beta in v'; constant_vector_simpl v'
+  | t _ 0 => eapply case0 with (v:=vec)
+  end.
+
+Lemma sndkey_of_append (v1 v2 : Vector.t _ 4) : sndkey (v1 ++ v2) = v2.
+Proof.
+  cbv [sndkey slice_by_position]. rewrite !resize_default_id.
+  rewrite splitat_append. cbn [fst snd].
+  constant_vector_simpl v2. reflexivity.
+Qed.
+
+Lemma fstkey_of_append (v1 v2 : Vector.t _ 4) : fstkey (v1 ++ v2) = v1.
+Proof. constant_vector_simpl v1. reflexivity. Qed.
+
+Lemma fstkey_sndkey_append kp : fstkey kp ++ sndkey kp = kp.
+Proof. constant_vector_simpl kp. reflexivity. Qed.
+
+Lemma swap_keys_involutive kp : swap_keys (swap_keys kp) = kp.
+Proof. constant_vector_simpl kp. reflexivity. Qed.
+
+Definition projkey1 (x : rconst * keypair) : key :=
+  transpose_rev (fstkey (snd x)).
+Definition projkey2 (x : rconst * keypair) : key :=
+  transpose_rev (sndkey (snd x)).
+
+(* Adds dummy rconst + other key to a key to create a rconst * keypair *)
+Definition unprojkey1 (k : key) : rconst * keypair :=
+  (nat_to_byte 0, transpose_rev k ++ const (const (const false 8) 4) 4).
+Definition unprojkey2 (k : key) : rconst * keypair :=
+  (nat_to_byte 0, const (const (const false 8) 4) 4 ++ transpose_rev k).
+Lemma proj_unproj_key1 k : projkey1 (unprojkey1 k) = k.
+Proof.
+  cbv [projkey1 unprojkey1]. cbn [fst snd].
+  rewrite fstkey_of_append.
+  apply transpose_rev_involutive.
+Qed.
+
+Lemma proj_unproj_key2 k : projkey2 (unprojkey2 k) = k.
+Proof.
+  cbv [projkey2 unprojkey2]. cbn [fst snd].
+  rewrite sndkey_of_append.
+  apply transpose_rev_involutive.
+Qed.
--- silveroak-opentitan/aes/Spec/InterleavedInverseCipher.v
+++ silveroak-opentitan/aes/Spec/InterleavedInverseCipher.v
@@ -65,23 +65,13 @@ Section Spec.
     st.
 
   Section Equivalence.
-    Context (Nr : nat) (initial_key : key) (final_key : key)
+    Context (Nr : nat) (initial_key : key)
             (first_key : key) (middle_keys : list key) (last_key : key).
-    Context (key_expand : nat -> key -> key).
 
     (* key list matches all_keys *)
     Context (all_keys_eq :
-               all_keys key_expand Nr initial_key
+               all_keys inv_key_expand Nr initial_key
                = first_key :: middle_keys ++ [last_key]).
-
-    Context (final_key_correct :
-               forall k,
-                 last (all_keys key_expand Nr initial_key) k = final_key).
-
-    (* inv_key_expand is the inverse of key_expand *)
-    Context (inv_key_expand_key_expand :
-               forall i k,
-                 inv_key_expand (Nr - S i) (key_expand i k) = k).
 
     Let equivalent_inverse_cipher :=
       equivalent_inverse_cipher state key add_round_key
@@ -89,47 +79,18 @@ Section Spec.
 
     (* Interleaved inverse cipher is equivalent to original inverse cipher *)
     Lemma equivalent_inverse_cipher_interleaved_equiv input :
-      equivalent_inverse_cipher_interleaved Nr final_key input =
+      equivalent_inverse_cipher_interleaved Nr initial_key input =
       equivalent_inverse_cipher
-        last_key first_key (map inv_mix_columns_key (rev middle_keys)) input.
+        first_key last_key (map inv_mix_columns_key middle_keys) input.
     Proof.
       intros. subst equivalent_inverse_cipher.
 
-      pose proof (all_keys_inv_eq key_expand inv_key_expand Nr initial_key final_key
-                                  final_key_correct inv_key_expand_key_expand)
-        as inv_expand_keys_rev.
-
-      (* get the rcons/key pairs *)
       pose proof all_keys_eq as Hall_keys.
-      map_inversion Hall_keys; subst.
-      match goal with H : @eq (list key) _ (_ :: _ ++ [_]) |- _ =>
-                      rename H into Hall_keys end.
-
-      match type of Hall_keys with
-      | ?ls1 = ?ls2 =>
-        let H := fresh in
-        rename Hall_keys into H;
-          assert (Hall_keys: rev ls1 = rev ls2) by (rewrite H; reflexivity)
-      end.
-      rewrite <-inv_expand_keys_rev in Hall_keys.
-      repeat first [ progress cbn [rev app] in Hall_keys
-                   | rewrite rev_app_distr in Hall_keys ].
 
       cbv [equivalent_inverse_cipher_interleaved
+             equivalent_inverse_cipher_round_interleaved
              Cipher.equivalent_inverse_cipher].
-
-      (* remove the pair-match in the round *)
-      erewrite fold_left_ext with
-          (f:=equivalent_inverse_cipher_round_interleaved)
-        by (cbv [equivalent_inverse_cipher_round_interleaved];
-            intros; repeat destruct_pair_let; cbn [fst snd];
-            rewrite <-!surjective_pairing; reflexivity).
-      cbn [fst snd].
-
-      (* This specific form of the default for nth is designed to make the
-         map_nth rewrites work out *)
-      set (d:=inv_mix_columns_key final_key).
-      rewrite fold_left_to_seq with (default:=d).
+      rewrite fold_left_to_seq with (default:=initial_key).
       pose proof (length_all_keys _ _ _ _ _ Hall_keys).
       autorewrite with push_length in *.
 
@@ -142,16 +103,16 @@ Section Spec.
         replace (length x) with n by lia
       end.
 
-      (* extract first step from cipher so loops are in sync *)
-      cbn [rev seq fold_left]. rewrite Nat.eqb_refl.
+      (* extract first step from cipher_alt so loops are in sync *)
+      cbn [List.seq fold_left]. rewrite Nat.eqb_refl.
       rewrite <-seq_shift, fold_left_map.
 
       (* state the relationship between the two loop states (invariant) *)
       lazymatch goal with
-      | H : all_keys inv_key_expand ?n ?k = _ |- _ =>
+      | H : all_keys ?key_expand ?n ?k = _ |- _ =>
         pose (R:= fun (i : nat) (x : key * state) (y : state) =>
                     x = (nth (S i)
-                             (all_keys inv_key_expand n k) k, y))
+                             (all_keys key_expand n k) k, y))
       end.
 
       (* find loops on each side *)
@@ -190,11 +151,11 @@ Section Spec.
         f_equal;
           [ rewrite !nth_all_keys_succ by lia;
             reflexivity | ].
-        f_equal; [ ]. subst d.
-        rewrite map_nth.
+        f_equal; [ ].
         rewrite Hall_keys. cbn [nth].
         rewrite app_nth1 by length_hammer.
-        reflexivity. }
+        rewrite <-(map_nth inv_mix_columns_key).
+        apply nth_indep. length_hammer. }
     Qed.
   End Equivalence.
 End Spec.
