9ec4c41012a0a7222e31b5819476869e5e58c4c4
Jade Philipoom <jadep@google.com>
2020-12-04 15:39:12 +0000

    make script simple wrapper

--- tools/minimize-requires.sh
+++ tools/minimize-requires.sh
@@ -15,43 +15,19 @@
 # limitations under the License.
 #
 
+# do not continue if failures are encountered
+set -e
+
 TOPLEVEL=$(git rev-parse --show-toplevel)
 
 # Do not use --absolutize here, because of https://github.com/JasonGross/coq-tools/issues/55
-MINIMIZE="$TOPLEVEL/third_party/coq-tools/minimize-requires.py --in-place"
+MINIMIZE="$TOPLEVEL/third_party/coq-tools/minimize-requires.py --in-place --all -f _CoqProject"
 
-MAKECOQ="make -j4 coq"
 
-COQPROJECTS=$(git ls-files --full-name $TOPLEVEL/*/ | grep _CoqProject | grep -v "^investigations/" | grep -v "^third_party")
+# TODO: remove if unneeded
+# LIBS=$(grep "^\s*-[RIQ]" _CoqProject | xargs echo) # get all library arguments from _CoqProject
+# FILES=$(grep "\.v$" _CoqProject | xargs echo) # get .v files from _CoqProject
 
-# do not continue loop if failures are encountered
-set -e
-
-# minimize-requires script analyzes .glob files, so we need to fully build first
-echo "Building all Coq files before minimizing..."
-$MAKECOQ
-
-for f in $COQPROJECTS;
-do
-	cd "$TOPLEVEL/$(dirname $f)";
-
-	# re-make in case we have gotten out of sync with another directory
-	$MAKECOQ
-
-	LIBS=$(grep "^\s*-[RIQ]" _CoqProject | xargs echo) # get all library arguments from _CoqProject
-
-	FILES=$(grep "\.v$" _CoqProject | xargs echo) # get .v files from _CoqProject
-
-	# get library arguments that do NOT reference parent directories
-	# (otherwise coqdep will include non-local files in the sorted list)
-	LOCALLIBS=$(grep "^\s*-[RIQ]" _CoqProject | grep -v "^\s*-[RIQ]\s\+\.\." | xargs echo)
-	FILES=$(coqdep -sort $LOCALLIBS $FILES) # sort using coqdep
-
-	echo "Minimizing imports in $PWD...";
-	echo "$MINIMIZE $LIBS $FILES"; # print command being executed (helpful for debugging)
-	$MINIMIZE $LIBS $FILES;
-
-	# re-make with fewer imports
-	echo "Re-building Coq files in $PWD...";
-	$MAKECOQ;
-done;
+echo "Minimizing imports in $PWD..."
+echo "$MINIMIZE" # print command being executed (helpful for debugging)
+$MINIMIZE
