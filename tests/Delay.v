344b2990709c97b82c5a3a4e72577ac1cfa98bbf
jadephilipoom <jadep@google.com>
2020-12-04 16:16:24 +0000

    Count by proofs (#372)
    * proof for countBy in terms of addN proof
    
    * automate proofs

--- tests/Delay.v
+++ tests/Delay.v
@@ -22,9 +22,12 @@ Import ListNotations.
 Require Import ExtLib.Structures.Monads.
 Export MonadNotation.
 
+Require Import Cava.Acorn.Identity.
 Require Import Cava.Cava.
+Require Import Cava.Tactics.
 Require Import Cava.Monad.CavaMonad.
 Require Import Cava.Lib.UnsignedAdders.
+
 
 From Coq Require Vector.
 From Coq Require Import Bool.Bvector.
@@ -142,7 +145,7 @@ bit-growth i.e. it computes (a + b) mod 256.
 *)
 
 Section WithCava.
-  Context {signal} `{Cava signal} `{Monad cava}.
+  Context {signal} {semantics:Cava signal} `{Monad cava}.
 
   Definition countFork (i : signal (Vec Bit 8) * signal (Vec Bit 8))
                        : cava (signal (Vec Bit 8) * signal (Vec Bit 8)) :=
@@ -159,16 +162,62 @@ Proof. reflexivity. Qed.
 
 Local Open Scope N_scope.
 
-Fixpoint countBySpec' (state: N) (i : list (Bvector 8)) 
+Fixpoint countBySpec' (state: Bvector 8) (i : list (Bvector 8))
                       : list (Bvector 8) :=
   match i with
   | [] => []
-  | x::xs => let newState := (Bv2N x + state) mod 256 in
-             N2Bv_sized 8 newState :: countBySpec' newState xs 
+  | x::xs =>
+    let newState := N2Bv_sized 8 (Bv2N x + Bv2N state) in
+    newState :: countBySpec' newState xs
   end.
 
-Definition countBySpec := countBySpec' 0.
+Definition countBySpec := countBySpec' (N2Bv_sized 8 0).
+
+(* TODO: addN sequential seems to only return one result; shouldn't it be a map2? *)
+Definition addNSpec {n} (a b : list (Bvector n)) : list (Bvector n) :=
+  match a,b with
+  | a :: _, b :: _ => [N2Bv_sized n ((Bv2N a + Bv2N b))]
+  | _,_ => []
+  end.
+
+Local Ltac seqsimpl_step :=
+  first [ progress cbn beta iota delta
+                   [fst snd hd sequential loopSeq' loop SequentialCombSemantics]
+        | progress cbv beta iota delta [loopSeq]; seqsimpl_step
+        | progress autorewrite with seqsimpl
+        | progress destruct_pair_let
+        | progress simpl_ident ].
+Local Ltac seqsimpl := repeat seqsimpl_step.
+
+(* TODO: rename typeclass arguments *)
+Lemma addNCorrect n (a b : list (Bvector n)) :
+  sequential (addN (H:=SequentialCombSemantics) a b) = addNSpec a b.
+Admitted.
+Hint Rewrite addNCorrect using solve [eauto] : seqsimpl.
+
+Lemma countForkStep:
+  forall (i : Bvector 8) (s : Bvector 8),
+    sequential (countFork (semantics:=SequentialCombSemantics) ([i], [s]))
+    = (countBySpec' s [i], countBySpec' s [i]).
+Proof.
+  intros; cbv [countFork countBySpec'].
+  seqsimpl; reflexivity.
+Qed.
+Hint Rewrite countForkStep using solve [eauto] : seqsimpl.
+
+Lemma countForkCorrect:
+  forall (i : list (Bvector 8)) (s : Bvector 8),
+    sequential (loopSeq' (countFork (semantics:=SequentialCombSemantics)) i [s])
+    = countBySpec' s i.
+Proof.
+  cbv [sequential]; induction i; intros; [ reflexivity | ].
+  seqsimpl. cbn [countBySpec']; rewrite IHi; reflexivity.
+Qed.
+Hint Rewrite countForkCorrect using solve [eauto] : seqsimpl.
 
 Lemma countByCorrect: forall (i : list (Bvector 8)),
                       sequential (countBy i) = countBySpec i.
-Abort.                      
+Proof.
+  intros; cbv [countBy countBySpec].
+  seqsimpl; reflexivity.
+Qed.
