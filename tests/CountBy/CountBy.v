585e994e42ea23308021176dffc6f775f99a41b8
Jade Philipoom <jadep@google.com>
2020-12-07 19:32:16 +0000

    move count-by proofs into their own file

--- tests/CountBy/CountBy.v
+++ tests/CountBy/CountBy.v
@@ -26,9 +26,6 @@ Require Import Cava.Tactics.
 Require Import Cava.Monad.CavaMonad.
 Require Import Cava.Lib.UnsignedAdders.
 
-From Coq Require Import Bool.Bvector.
-
-
 (******************************************************************************)
 (* countBy                                                                    *)
 (******************************************************************************) 
@@ -75,68 +72,5 @@ Definition b250 := N2Bv_sized 8 250.
 
 Local Open Scope list_scope.
 
-
 Example countBy_ex1: sequential (countBy [b14; b7; b3; b250]) = [b14; b21; b24; b18].
 Proof. reflexivity. Qed.
-
-Local Open Scope N_scope.
-
-Fixpoint countBySpec' (state: Bvector 8) (i : list (Bvector 8))
-                      : list (Bvector 8) :=
-  match i with
-  | [] => []
-  | x::xs =>
-    let newState := N2Bv_sized 8 (Bv2N x + Bv2N state) in
-    newState :: countBySpec' newState xs
-  end.
-
-Definition countBySpec := countBySpec' (N2Bv_sized 8 0).
-
-(* TODO: addN sequential seems to only return one result; shouldn't it be a map2? *)
-Definition addNSpec {n} (a b : list (Bvector n)) : list (Bvector n) :=
-  match a,b with
-  | a :: _, b :: _ => [N2Bv_sized n ((Bv2N a + Bv2N b))]
-  | _,_ => []
-  end.
-
-Local Ltac seqsimpl_step :=
-  first [ progress cbn beta iota delta
-                   [fst snd hd sequential loopSeq' loop SequentialCombSemantics]
-        | progress cbv beta iota delta [loopSeq]; seqsimpl_step
-        | progress autorewrite with seqsimpl
-        | progress destruct_pair_let
-        | progress simpl_ident ].
-Local Ltac seqsimpl := repeat seqsimpl_step.
-
-(* TODO: rename typeclass arguments *)
-Lemma addNCorrect n (a b : list (Bvector n)) :
-  sequential (addN (H:=SequentialCombSemantics) a b) = addNSpec a b.
-Admitted.
-Hint Rewrite addNCorrect using solve [eauto] : seqsimpl.
-
-Lemma countForkStep:
-  forall (i : Bvector 8) (s : Bvector 8),
-    sequential (countFork (semantics:=SequentialCombSemantics) ([i], [s]))
-    = (countBySpec' s [i], countBySpec' s [i]).
-Proof.
-  intros; cbv [countFork countBySpec'].
-  seqsimpl; reflexivity.
-Qed.
-Hint Rewrite countForkStep using solve [eauto] : seqsimpl.
-
-Lemma countForkCorrect:
-  forall (i : list (Bvector 8)) (s : Bvector 8),
-    sequential (loopSeq' (countFork (semantics:=SequentialCombSemantics)) i [s])
-    = countBySpec' s i.
-Proof.
-  cbv [sequential]; induction i; intros; [ reflexivity | ].
-  seqsimpl. cbn [countBySpec']; rewrite IHi; reflexivity.
-Qed.
-Hint Rewrite countForkCorrect using solve [eauto] : seqsimpl.
-
-Lemma countByCorrect: forall (i : list (Bvector 8)),
-                      sequential (countBy i) = countBySpec i.
-Proof.
-  intros; cbv [countBy countBySpec].
-  seqsimpl; reflexivity.
-Qed.
