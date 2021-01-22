633cabbaf673e5af73966c7ea2accc779f2b955a
Ben Blaxill <benblaxill@google.com>
2021-01-22 18:00:39 +0000

    Add Acorn aes_shift_rows (#454)

---
+++ silveroak-opentitan/aes/Acorn/ShiftRows.v
@@ -0,0 +1,100 @@
+(****************************************************************************)
+(* Copyright 2020 The Project Oak Authors                                   *)
+(*                                                                          *)
+(* Licensed under the Apache License, Version 2.0 (the "License")           *)
+(* you may not use this file except in compliance with the License.         *)
+(* You may obtain a copy of the License at                                  *)
+(*                                                                          *)
+(*     http://www.apache.org/licenses/LICENSE-2.0                           *)
+(*                                                                          *)
+(* Unless required by applicable law or agreed to in writing, software      *)
+(* distributed under the License is distributed on an "AS IS" BASIS,        *)
+(* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *)
+(* See the License for the specific language governing permissions and      *)
+(* limitations under the License.                                           *)
+(****************************************************************************)
+
+Require Import Coq.Vectors.Vector.
+
+Require Import ExtLib.Structures.Monads.
+Require Import ExtLib.Structures.Traversable.
+
+Require Import Cava.VectorUtils.
+Require Import Cava.Acorn.Acorn.
+Require Import Cava.Lib.BitVectorOps.
+Require Import AcornAes.Common.
+Require Import AcornAes.Pkg.
+Require Import AesSpec.Tests.CipherTest.
+Require Import AesSpec.Tests.Common.
+Require Import AesSpec.StateTypeConversions.
+Import Common.Notations.
+
+Import VectorNotations.
+
+Local Notation byte := (Vec Bit 8) (only parsing).
+Local Notation "v [@ n ]" := (indexConst v n) (at level 1, format "v [@ n ]").
+
+Section WithCava.
+  Context {signal} {semantics : Cava signal}.
+  Context {monad: Monad cava}.
+
+  Definition aes_circ_byte_shift (shift: nat) (input: signal (Vec byte 4)):
+    cava (signal (Vec byte 4)) :=
+    let indices := [4 - shift; 5 - shift; 6 - shift; 7 - shift] in
+    let indices := map (fun x => Nat.modulo x 4) indices in
+    ret (unpeel (map (indexConst input) indices))
+    .
+
+  Definition aes_shift_rows
+    (op_i: signal Bit)
+    (data_i: signal (Vec (Vec byte 4) 4))
+    : cava (signal (Vec (Vec byte 4) 4)) :=
+  (* // Row 0 is left untouched *)
+  (* assign data_o[0] = data_i[0]; *)
+  let data_o_0 := data_i[@0] in
+  (* // Row 2 does not depend on op_i *)
+  (* assign data_o[2] = aes_circ_byte_shift(data_i[2], 2'h2); *)
+  data_o_2 <- aes_circ_byte_shift 2 data_i[@2] ;;
+
+  (* // Row 1 *)
+  (* assign data_o[1] = (op_i == CIPH_FWD) ? aes_circ_byte_shift(data_i[1], 2'h3) *)
+  (*                                       : aes_circ_byte_shift(data_i[1], 2'h1); *)
+  data_o_1_0 <- aes_circ_byte_shift 3 data_i[@1] ;;
+  data_o_1_1 <- aes_circ_byte_shift 1 data_i[@1] ;;
+  data_o_1 <- muxPair op_i (data_o_1_0, data_o_1_1) ;;
+
+  (* // Row 3 *)
+  (* assign data_o[3] = (op_i == CIPH_FWD) ? aes_circ_byte_shift(data_i[3], 2'h1) *)
+  (*                                       : aes_circ_byte_shift(data_i[3], 2'h3); *)
+  data_o_3_0 <- aes_circ_byte_shift 1 data_i[@3] ;;
+  data_o_3_1 <- aes_circ_byte_shift 3 data_i[@3] ;;
+  data_o_3 <- muxPair op_i (data_o_3_0, data_o_3_1) ;;
+
+  ret (unpeel [data_o_0; data_o_1; data_o_2; data_o_3]).
+
+End WithCava.
+
+(* Run test as a quick-feedback check *)
+Require Import Cava.BitArithmetic.
+Require Import AesSpec.StateTypeConversions.
+
+Import List.ListNotations.
+Goal
+  (let signal := combType in
+  let to_state : Vector.t bool 128 -> signal state :=
+      fun st => Vector.map (Vector.map (fun r => byte_to_bitvec r)) (BigEndian.to_rows st) in
+  let from_state : signal state -> Vector.t bool 128 :=
+      fun st => BigEndian.from_rows (Vector.map (Vector.map (fun r => bitvec_to_byte r)) st) in
+
+   (* run encrypt test with this version of aes_mix_columns plugged in *)
+   aes_test_encrypt Matrix
+                    (fun step key =>
+                       match step with
+                       | ShiftRows =>
+                         fun st =>
+                           let input := to_state st in
+                           let output := unIdent (aes_shift_rows [false]%list [input]%list) in
+                           from_state (List.hd (defaultCombValue _) output)
+                       | _ => aes_impl step key
+                       end) = Success).
+Proof. vm_compute. reflexivity. Qed.
