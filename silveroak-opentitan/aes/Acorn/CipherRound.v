0893080df5ed096ade48f5e5c29319e3a795f885
Jade Philipoom <jadep@google.com>
2021-01-26 16:49:13 +0000

    make cipher implementation use loop

--- silveroak-opentitan/aes/Acorn/CipherRound.v
+++ silveroak-opentitan/aes/Acorn/CipherRound.v
@@ -31,7 +31,8 @@ Import Common.Notations.
 Local Open Scope monad_scope.
 
 Section WithCava.
-  Context {signal} {semantics : Cava signal} {monad: Monad cava}.
+  Context {signal} {semantics : Cava signal} {seqsemantics : CavaSeq semantics}
+          {monad: Monad cava}.
   Context {round_index round_constant : SignalType}.
 
   Context (sub_bytes:     signal Bit -> signal state -> cava (signal state))
@@ -56,19 +57,20 @@ Section WithCava.
 
   Definition key_expand_and_round
              (is_decrypt : signal Bit)
-             (key_rcon_input : signal key * signal round_constant * signal state)
+             (key_rcon_data : signal (Pair (Pair key round_constant) state))
              (add_round_key_in_sel : signal (Vec Bit 2))
              (round_key_sel : signal Bit)
              (round_i : signal round_index)
-    : cava (signal key * signal round_constant * signal state) :=
-    let '(round_key, rcon, input) := key_rcon_input in
-    shift_rows_out <- (sub_bytes is_decrypt >=> shift_rows is_decrypt) input ;;
+    : cava (signal (Pair (Pair key round_constant) state)) :=
+    let '(key_rcon, data) := unpair key_rcon_data in
+    let '(round_key, rcon) := unpair key_rcon in
+    shift_rows_out <- (sub_bytes is_decrypt >=> shift_rows is_decrypt) data ;;
     mix_columns_out <- mix_columns is_decrypt shift_rows_out ;;
 
     (* Different rounds perform different operations on the state before adding
        the round key; select the appropriate wire based on add_round_key_in_sel *)
     let add_round_key_in :=
-        mux4 (mkpair (mkpair (mkpair mix_columns_out input) shift_rows_out) mix_columns_out)
+        mux4 (mkpair (mkpair (mkpair mix_columns_out data) shift_rows_out) mix_columns_out)
              add_round_key_in_sel in
 
     (* Intermediate decryption rounds need to mix the key columns *)
@@ -80,14 +82,15 @@ Section WithCava.
     (* Key expansion *)
     '(round_key, rcon) <- key_expand is_decrypt round_i (round_key, rcon) ;;
 
-    ret (round_key, rcon, out).
+    ret (mkpair (mkpair round_key rcon) out).
 
   Definition cipher_step
              (is_decrypt : signal Bit) (* called op_i in OpenTitan *)
-             (key_rcon_data : signal key * signal round_constant * signal state)
+             (key_rcon_data : signal (Pair (Pair key round_constant) state))
              (round_i : signal round_index)
-    : cava (signal key * signal round_constant * signal state) :=
-    let '(round_key, rcon, input) := key_rcon_data in
+    : cava (signal (Pair (Pair key round_constant) state)) :=
+    let '(key_rcon, data) := unpair key_rcon_data in
+    let '(round_key, rcon) := unpair key_rcon in
     is_first_round <- round_i ==? round_0 ;;
     is_final_round <- round_i ==? num_rounds_regular ;;
     (* add_round_key_in_sel :
@@ -96,16 +99,24 @@ Section WithCava.
     is_middle_round <- nor2 (is_first_round, is_final_round) ;;
     (* round_key_sel : 1 for a decryption middle round, 0 otherwise *)
     round_key_sel <- and2 (is_middle_round, is_decrypt) ;;
-    key_expand_and_round is_decrypt (round_key, rcon, input)
+    key_expand_and_round is_decrypt key_rcon_data
                          add_round_key_in_sel round_key_sel round_i.
 
   Definition cipher
              (is_decrypt : signal Bit) (* called op_i in OpenTitan *)
              (initial_key : signal key) (initial_rcon : signal round_constant)
-             (round_indices : list (signal round_index)) (input : signal state)
+             (round_i : signal round_index) (input : signal state)
     : cava (signal state) :=
-    '(_, _, out) <- foldLM (cipher_step is_decrypt) round_indices
-                          (initial_key, initial_rcon, input) ;;
-    ret out.
+    let initial_state := mkpair (mkpair initial_key initial_rcon) input in
+    loop_out <- loopDelayS
+                 (fun index_and_state : signal round_index
+                                      * signal (Pair (Pair key round_constant) state) =>
+                    let '(idx, feedback_state) := index_and_state in
+                    is_first_round <- idx ==? round_0 ;;
+                    cipher_state <- muxPair is_first_round (initial_state, feedback_state) ;;
+                    cipher_step is_decrypt cipher_state idx)
+                 round_i ;;
+    let '(_,final_data) := unpair loop_out in
+    ret final_data.
 
 End WithCava.
