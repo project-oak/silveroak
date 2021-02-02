fdff3a75489c0fb2e191310a969750453e794e63
Satnam Singh <satnam@google.com>
2021-02-02 14:52:15 -0800

    Generate hex literals

--- cava/Cava2SystemVerilog.hs
+++ cava/Cava2SystemVerilog.hs
@@ -127,15 +127,34 @@ showVectorElements :: [Signal] -> String
 showVectorElements e
   = concat (insertCommas (map showSignal e))
 
+
+isGndOrVcc :: Signal -> Bool
+isGndOrVcc Gnd = True
+isGndOrVcc Vcc = True
+isGndOrVcc _ = False
+
+gndVccListToInt :: [Signal] -> Int
+gndVccListToInt [] = 0
+gndVccListToInt (Gnd:xs) = 2 * gndVccListToInt xs
+gndVccListToInt (Vcc:xs) = 1 + 2 * gndVccListToInt xs
+
+bvConst :: [Signal] -> String
+bvConst bv
+  = show (length bv) ++ "'h" ++ showHex (gndVccListToInt bv) ""
+
 -- Show vector literals, interpretating them as packed arrays
 -- with downto indexing.
 showVecLiteral :: SignalType -> [Signal] -> String
 showVecLiteral k e
   = case k of
-      Bit -> -- Packed vector, downto indexing
+      Bit -> if all isGndOrVcc e then  -- special case for bit-vector constants
+               bvConst e
+             else
+              -- Vector, downto indexing
+              "{" ++ showVectorElements (reverse e) ++ "}"
+      _   -> -- Vector, downto indexing
              "{" ++ showVectorElements (reverse e) ++ "}"
-      _   -> -- unpacked vector, downto indexing
-             "'{" ++ showVectorElements (reverse e) ++ "}"
+
 showSignal :: Signal -> String
 showSignal signal
   = case signal of
