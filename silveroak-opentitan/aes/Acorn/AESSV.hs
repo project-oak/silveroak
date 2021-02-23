a042e7ef55679df536581f99f6b779dbcc9f629f
Ben Blaxill <benblaxill@google.com>
2021-02-23 10:51:56 +0000

    Generate aes_cipher_core netlist (#576)

--- silveroak-opentitan/aes/Acorn/AESSV.hs
+++ silveroak-opentitan/aes/Acorn/AESSV.hs
@@ -33,3 +33,4 @@ main = do writeSystemVerilog aes_mix_columns_Netlist
           writeSystemVerilog aes_add_round_key_Netlist
           writeTestBench aes_add_round_key_tb
           writeSystemVerilog key_expand_and_round_Netlist
+          writeSystemVerilog aes_cipher_core_simplified_Netlist
