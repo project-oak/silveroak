19b53c11230cbb8e804ddd90413a430f193944ec
Ben Blaxill <benblaxill@google.com>
2021-03-16 16:50:23 +0000

    Update aes bash scripts (#646)

--- silveroak-opentitan/aes/copy_to_opentitan.sh
+++ silveroak-opentitan/aes/copy_to_opentitan.sh
@@ -21,23 +21,31 @@ set -eu
 OPENTITAN_AES_DIR=../../third_party/opentitan/hw/ip/aes/rtl
 VERILATOR_CONFIG=../../third_party/opentitan/hw/lint/tools/verilator/common.vlt
 
-# Comment out timing
-awk '{ if ($0 ~ /timeprecision/) { print "/*" $0 "*/" } else { print $0 } }' Impl/aes_mix_columns.sv > aes_mix_columns.sv
-awk '{ if ($0 ~ /timeprecision/) { print "/*" $0 "*/" } else { print $0 } }' Impl/aes_sbox_lut.sv > aes_sbox_lut.sv
-awk '{ if ($0 ~ /timeprecision/) { print "/*" $0 "*/" } else { print $0 } }' Impl/aes_shift_rows.sv > aes_shift_rows.sv
-awk '{ if ($0 ~ /timeprecision/) { print "/*" $0 "*/" } else { print $0 } }' Impl/aes_cipher_core.sv > aes_cipher_core.sv
+# Remove timing as OpenTitan modules do not have them and Verilator raises
+# errors with mixed usage
+awk '!/timeprecision/ { print }' Impl/aes_mix_columns.sv > aes_mix_columns.sv
+awk '!/timeprecision/ { print }' Impl/aes_sbox_lut.sv > aes_sbox_lut.sv
+awk '!/timeprecision/ { print }' Impl/aes_shift_rows.sv > aes_shift_rows.sv
 
-# For aes_sub_bytes additionally add a dummy parameter to module definition
-awk '{ \
-  if ($0 ~ /module aes_sub_bytes/) {print "module aes_sub_bytes #( parameter SBoxImpl = \"lut\") ("} \
-  else { \
-    if ($0 ~ /timeprecision/) { print "/*" $0 "*/" } else { print $0 } \
-  } }' Impl/aes_sub_bytes.sv > aes_sub_bytes.sv
+# Add dummy parameters for aes_sub_bytes and aes_cipher_core
+awk '
+  { switch ($0) {
+    case /module aes_sub_bytes/: {print "module aes_sub_bytes #( parameter SBoxImpl = \"lut\") ("}; break
+    case /timeprecision/: break
+    default: print; break
+  } } ' Impl/aes_sub_bytes.sv > aes_sub_bytes.sv
+
+awk '
+  { switch ($0) {
+    case /module aes_cipher_core/: {print "module aes_cipher_core #( parameter bit AES192Enable = 1, parameter SBoxImpl = \"lut\") ("}; break
+    case /timeprecision/: break
+    default: print; break
+  } } ' Impl/aes_cipher_core.sv > aes_cipher_core.sv
 
 cp aes_mix_columns.sv aes_sbox_lut.sv aes_sub_bytes.sv aes_shift_rows.sv aes_cipher_core.sv $OPENTITAN_AES_DIR
 
-# OpenTitan Verilator is empty, we need to turn off "DETECTARRAYS" as we
-# generate large arrays that choke this Verilator pass. Alternatively we could
+# OpenTitan Verilator config is empty, we need to turn off "DETECTARRAYS" as we
+# generate large arrays that choke Verilator. Alternatively we could
 # rebuild Verilator with larger inbuilt DETECTARRAYS constant
 cat <<EOS > $VERILATOR_CONFIG
 \`verilator_config
