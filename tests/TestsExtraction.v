f90a3f13cea5fe863d65db376d6d1036555a7dec
Anish Tondwalkar <atondwal@google.com>
2021-03-25 09:04:31 -0700

    Add encoder/decoder  (#685)
    fixes #672

--- tests/TestsExtraction.v
+++ tests/TestsExtraction.v
@@ -31,6 +31,7 @@ Require Import Tests.DoubleCountBy.DoubleCountBy.
 Require Import Tests.AccumulatingAdderEnable.AccumulatingAdderEnable.
 Require Import Tests.Array.
 Require Import Tests.TestVecConstEq.
+Require Import Tests.TestDecoder.
 
 Extraction Library Instantiate.
 Extraction Library MuxTests.
@@ -41,3 +42,4 @@ Extraction Library CountBy.
 Extraction Library DoubleCountBy.
 Extraction Library Array.
 Extraction Library TestVecConstEq.
+Extraction Library TestDecoder.
