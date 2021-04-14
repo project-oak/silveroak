3faca045e2d2748cb1ba84b61d1166ddc9eccdf7
Anish Tondwalkar <atondwal@google.com>
2021-04-14 19:18:15 -0400

    investigations/vector : Added proofs about InV (#740)
    There are the proofs that didn't end up being used for DecoderProperties

---
+++ investigations/vector/In.v
@@ -0,0 +1,103 @@
+(****************************************************************************)
+(* Copyright 2021 The Project Oak Authors                                   *)
+(*                                                                          *)
+(* Licensed under the Apache License, Version 2.0 (the "License")           *)
+(* you may not use this file except in compliance with the License.         *)
+(* You may obtain a copy of the License at                                  *)
+(*                                                                          *)
+(*     http://www.apache.org/licenses/LICENSE-2.0                           *)
+(*                                                                          *)
+(* Unless required by applicable law or agreed to in writing, software      *)
+(* distributed under the License is distributed on an "AS IS" BASIS,        *)
+(* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *)
+(* See the License for the specific language governing permissions and      *)
+(* limitations under the License.                                           *)
+(****************************************************************************)
+
+Require Import Cava.CavaProperties.
+
+Theorem In_map :
+  forall A B (n : nat) (a : A) f (v : Vector.t B n),
+    (exists x, f x = a /\ Vector.In x v) ->  Vector.In a (Vector.map f v).
+Proof.
+  intros.
+  destruct H.
+  destruct H.
+  induction v.
+  { inversion H0. }
+  { inversion H0.
+    { cbn. subst. apply Vector.In_cons_hd. }
+    { cbn.
+      apply Eqdep_dec.inj_pair2_eq_dec in H4.
+      2:apply Nat.eq_dec.
+      apply Vector.In_cons_tl.
+      apply IHv.
+      subst.
+      trivial. }
+  }
+Qed.
+
+Theorem In_list:
+  forall n len start : nat, Vector.In n (vseq start len) <-> List.In n (seq start len).
+Proof.
+  induction len.
+  { compute.
+    split; intros; inversion H. }
+  { split.
+    { intros.
+      inversion H.
+      { left. trivial. }
+      { apply Eqdep_dec.inj_pair2_eq_dec in H3.
+        2: apply Nat.eq_dec.
+        subst.
+        right.
+        apply IHlen.
+        rewrite Nat.add_comm in H2.
+        assumption. }
+    }
+    { cbn [In seq].
+      intros [Hl | Hr].
+      { subst. apply Vector.In_cons_hd. }
+      { simpl.
+        apply Vector.In_cons_tl.
+        apply IHlen.
+        rewrite Nat.add_comm.
+        apply Hr. }
+    }
+  }
+Qed.
+
+Lemma In_destruct : forall n A (a h:A) (v : Vector.t A n),
+  Vector.In a (h :: v) -> a = h \/ Vector.In a v.
+Proof.
+  intros.
+  inversion H.
+  { left. trivial. }
+  { apply Eqdep_dec.inj_pair2_eq_dec in H3.
+    2: apply Nat.eq_dec.
+    subst.
+    right.
+    assumption. }
+Qed.
+
+Theorem In_seq:
+  forall n start len : nat, Vector.In n (vseq start len) <-> start <= n < start + len.
+Proof.
+  intros.
+  rewrite In_list.
+  apply in_seq.
+Qed.
+
+Theorem InV_correct A n a (vec : Vector.t A n) : InV a vec <-> Vector.In a vec.
+Proof.
+  split.
+  { induction vec.
+    { intro H. inversion H. }
+    { intros. simpl in H. destruct H.
+      { subst. apply Vector.In_cons_hd. }
+      { apply Vector.In_cons_tl. apply IHvec. assumption. }
+    }
+  }
+  { intros.
+    induction H; cbn; [left | right]; trivial. }
+Qed.
