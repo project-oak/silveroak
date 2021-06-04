0fb12e149f8af0c7bb71f9d428a5ef481e6dabdc
Dayeol Lee <dayeol@berkeley.edu>
2021-06-04 04:19:54 -0700

    Transcribe mask ROM uart functions with bedrock2 (#804)

---
+++ investigations/bedrock2/Uart/UartToC.v
@@ -0,0 +1,37 @@
+Require Import Coq.Lists.List.
+Require Import Coq.Strings.String.
+Require Import Coq.ZArith.ZArith.
+Require Import bedrock2.Syntax.
+Require Import bedrock2.ToCString.
+Require Import bedrock2.Variables.
+Require Import coqutil.Z.HexNotation.
+Require Import Bedrock2Experiments.Uart.Uart.
+Require Import Bedrock2Experiments.LibBase.Bitfield.
+Require Import Bedrock2Experiments.LibBase.AbsMMIO.
+Require Import Bedrock2Experiments.Uart.Constants.
+Import ListNotations.
+Local Open Scope string_scope.
+Local Open Scope Z_scope.
+
+Existing Instance constant_names.
+
+Definition funcs := [
+  bitfield_field32_write
+  ;bitfield_field32_read
+  ;bitfield_bit32_read
+  ;bitfield_bit32_write
+  ;abs_mmio_write32
+  ;abs_mmio_read32
+  ;uart_reset
+  ;uart_init
+  ;uart_tx_full
+  ;uart_tx_idle
+  ;uart_putchar
+  ;uart_write
+  ;uart_sink
+  ].
+
+Definition make_uart_c :=
+  concat LF (map (fun f => "static " ++ c_func f) funcs).
+
+Redirect "uart.c" Compute make_uart_c.
