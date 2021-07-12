4af73c59d0fbc57f80f96059149427077762de05
Jade Philipoom <jadep@google.com>
2021-07-12 16:49:56 +0100

    WIP

--- cava/Cava/Lib/CircuitTransformsProperties.v
+++ cava/Cava/Lib/CircuitTransformsProperties.v
@@ -16,6 +16,7 @@
 
 Require Import Coq.Classes.Morphisms.
 Require Import coqutil.Tactics.Tactics.
+Require Import ExtLib.Structures.Monad.
 Require Import Cava.Core.Core.
 Require Import Cava.Semantics.Combinational.
 Require Import Cava.Semantics.Equivalence.
@@ -26,7 +27,7 @@ Require Import Cava.Semantics.Simulation.
 Require Import Cava.Lib.CircuitTransforms.
 Require Import Cava.Util.List.
 Require Import Cava.Util.Tactics.
-Import Circuit.Notations.
+Import MonadNotation Circuit.Notations.
 
 Local Ltac simplify :=
   cbn [circuit_state step Id value]; intros;
@@ -34,19 +35,19 @@ Local Ltac simplify :=
   repeat (destruct_pair_let; cbn [fst snd]);
   logical_simplify; simpl_ident; subst.
 
-Lemma First_Id t1 t2 : @cequiv (t1 * t2) (t1 * t2) (First Id) Id.
+Lemma First_Id {t1 t2} : @cequiv (t1 * t2) (t1 * t2) (First Id) Id.
 Proof.
   exists eq. ssplit; [ reflexivity | ]. simplify. ssplit; reflexivity.
 Qed.
 Hint Rewrite @First_Id using solve [eauto] : circuitsimpl.
 
-Lemma Second_Id t1 t2 : @cequiv (t1 * t2) (t1 * t2) (Second Id) Id.
+Lemma Second_Id {t1 t2} : @cequiv (t1 * t2) (t1 * t2) (Second Id) Id.
 Proof.
   exists eq. ssplit; [ reflexivity | ]. simplify. ssplit; reflexivity.
 Qed.
 Hint Rewrite @Second_Id using solve [eauto] : circuitsimpl.
 
-Lemma Compose_Id_l i o c : @cequiv i o (Id >==> c) c.
+Lemma Compose_Id_l {i o} c : @cequiv i o (Id >==> c) c.
 Proof.
   exists (fun (st1 : unit * value (circuit_state c))
        (st2 : value (circuit_state c)) => st1 = (tt, st2)).
@@ -54,7 +55,7 @@ Proof.
 Qed.
 Hint Rewrite @Compose_Id_l using solve [eauto] : circuitsimpl.
 
-Lemma Compose_Id_r i o c : @cequiv i o (c >==> Id) c.
+Lemma Compose_Id_r {i o} c : @cequiv i o (c >==> Id) c.
 Proof.
   exists (fun (st1 : value (circuit_state c) * unit)
        (st2 : value (circuit_state c)) => st1 = (st2, tt)).
@@ -62,7 +63,7 @@ Proof.
 Qed.
 Hint Rewrite @Compose_Id_r using solve [eauto] : circuitsimpl.
 
-Lemma Compose_assoc A B C D
+Lemma Compose_assoc {A B C D}
       (f : Circuit A B) (g : Circuit B C) (h : Circuit C D) :
   cequiv (f >==> (g >==> h)) (f >==> g >==> h).
 Proof.
@@ -71,22 +72,23 @@ Proof.
        st1 = (fst (fst st2), (snd (fst st2), snd st2))).
   ssplit; [ reflexivity | ]. simplify. ssplit; reflexivity.
 Qed.
+Hint Rewrite @Compose_assoc : circuitsimpl.
 
-Lemma First_Compose A B C D (f : Circuit A B) (g : Circuit B C) :
+Lemma First_Compose {A B C D} (f : Circuit A B) (g : Circuit B C) :
   @cequiv (A * D) (C * D) (First (f >==> g)) (First f >==> First g).
 Proof.
   exists eq. ssplit; [ reflexivity | ]. simplify. ssplit; reflexivity.
 Qed.
 Hint Rewrite @First_Compose using solve [eauto] : circuitsimpl.
 
-Lemma Second_Compose A B C D (f : Circuit A B) (g : Circuit B C) :
+Lemma Second_Compose {A B C D} (f : Circuit A B) (g : Circuit B C) :
   @cequiv (D * A) (D * C) (Second (f >==> g)) (Second f >==> Second g).
 Proof.
   exists eq. ssplit; [ reflexivity | ]. simplify. ssplit; reflexivity.
 Qed.
 Hint Rewrite @Second_Compose using solve [eauto] : circuitsimpl.
 
-Lemma LoopInitCE_First_r A B C (s : SignalType) reset
+Lemma LoopInitCE_First_r {A B C} (s : SignalType) reset
       (body : Circuit (A * s) (B * s)) (f : Circuit B C) :
   cequiv (LoopInitCE reset (body >==> First f)) (LoopInitCE reset body >==> f).
 Proof.
@@ -97,7 +99,7 @@ Proof.
 Qed.
 Hint Rewrite @LoopInitCE_First_r using solve [eauto] : push_loop.
 
-Lemma LoopInit_First_r A B C (s : SignalType) reset
+Lemma LoopInit_First_r {A B C} (s : SignalType) reset
       (body : Circuit (A * s) (B * s)) (f : Circuit B C) :
   cequiv (LoopInit reset (body >==> First f)) (LoopInit reset body >==> f).
 Proof.
@@ -105,19 +107,19 @@ Proof.
 Qed.
 Hint Rewrite @LoopInit_First_r using solve [eauto] : push_loop.
 
-Lemma LoopCE_First_r A B C (s : SignalType)
+Lemma LoopCE_First_r {A B C} (s : SignalType)
       (body : Circuit (A * s) (B * s)) (f : Circuit B C) :
   cequiv (LoopCE (body >==> First f)) (LoopCE body >==> f).
 Proof. apply LoopInitCE_First_r. Qed.
 Hint Rewrite @LoopCE_First_r using solve [eauto] : push_loop.
 
-Lemma Loop_First_r A B C (s : SignalType)
+Lemma Loop_First_r {A B C} (s : SignalType)
       (body : Circuit (A * s) (B * s)) (f : Circuit B C) :
   cequiv (Loop (body >==> First f)) (Loop body >==> f).
 Proof. apply LoopInit_First_r. Qed.
 Hint Rewrite @Loop_First_r using solve [eauto] : push_loop.
 
-Lemma simulate_Id t input : simulate (i:=t) Id input = input.
+Lemma simulate_Id {t} input : simulate (i:=t) Id input = input.
 Proof.
   cbv [Id]; autorewrite with push_simulate.
   apply List.map_id_ext; intros.
@@ -125,7 +127,7 @@ Proof.
 Qed.
 Hint Rewrite @simulate_Id using solve [eauto] : push_simulate.
 
-Lemma simulate_Par A B C D c1 c2 input :
+Lemma simulate_Par {A B C D} c1 c2 input :
   simulate (@Par _ _ A B C D c1 c2) input
   = combine (simulate c1 (map fst input))
             (simulate c2 (map snd input)).
@@ -144,60 +146,68 @@ Proof.
   reflexivity.
 Qed.
 
-Lemma First_Id_w t1 t2 : @wequiv (t1 * t2) (t1 * t2) (First Id) Id.
+Lemma Par_Id_l {i o t} (f : Circuit i o) : cequiv (Par f (Id (t:=t))) (First f).
+Proof. cbv [Par]. autorewrite with circuitsimpl. reflexivity. Qed.
+Hint Rewrite @Par_Id_l using solve [eauto] : circuitsimpl.
+
+Lemma Par_Id_r {i o t} (f : Circuit i o) : cequiv (Par (Id (t:=t)) f) (Second f).
+Proof. cbv [Par]. autorewrite with circuitsimpl. reflexivity. Qed.
+Hint Rewrite @Par_Id_r using solve [eauto] : circuitsimpl.
+
+Lemma Second_Comb {i o t} f :
+  cequiv (Second (Comb f)) (Comb (i:=t*i) (o:=t*o) (fun x => (fst x, f (snd x)))).
 Proof.
-  split; [ reflexivity | ]; intros.
-  cbn [cpath Id].
-  exists (fun i s1 s2 => i = 0 /\ s1 = s2); cbn [step circuit_state value Id fst snd].
-  simpl_ident. ssplit; intros; destruct_products;
-                 subst; ssplit; try reflexivity; discriminate.
+  exists (fun _ _ => True). ssplit; [ tauto | ].
+  cbn [value circuit_state]; intros. destruct_products.
+  repeat lazymatch goal with x : unit |- _ => destruct x end.
+  ssplit; reflexivity.
 Qed.
-Hint Rewrite @First_Id_w using solve [eauto] : circuitsimplw.
+Hint Rewrite @Second_Comb using solve [eauto] : pull_comb.
 
-Lemma Second_Id_w t1 t2 : @wequiv (t1 * t2) (t1 * t2) (Second Id) Id.
+Lemma First_Comb {i o t} f :
+  cequiv (First (Comb f)) (Comb (i:=i*t) (o:=o*t) (fun x => (f (fst x), snd x))).
 Proof.
-  split; [ reflexivity | ]; intros.
-  cbn [cpath Id].
-  exists (fun i s1 s2 => i = 0 /\ s1 = s2); cbn [step circuit_state value Id fst snd].
-  simpl_ident. ssplit; intros; destruct_products;
-                 subst; ssplit; try reflexivity; discriminate.
+  exists (fun _ _ => True). ssplit; [ tauto | ].
+  cbn [value circuit_state]; intros. destruct_products.
+  repeat lazymatch goal with x : unit |- _ => destruct x end.
+  ssplit; reflexivity.
 Qed.
-Hint Rewrite @Second_Id_w using solve [eauto] : circuitsimplw.
+Hint Rewrite @First_Comb using solve [eauto] : pull_comb.
 
-Lemma Compose_Id_l_w i o c : @wequiv i o (Id >==> c) c.
-Admitted.
-Hint Rewrite @Compose_Id_l_w using solve [eauto] : circuitsimplw.
-
-Lemma Compose_Id_r_w i o c : @wequiv i o (c >==> Id) c.
-Admitted.
-Hint Rewrite @Compose_Id_r_w using solve [eauto] : circuitsimplw.
-
-Lemma Par_Id_w t1 t2 : @wequiv (t1 * t2) (t1 * t2) (Par Id Id) Id.
+Lemma Compose_Comb {i t o} f g :
+  cequiv (@Compose _ _ i t o (Comb f) (Comb g)) (Comb (f >=> g)).
 Proof.
-  cbv [Par]. autorewrite with circuitsimplw. reflexivity.
+  exists (fun _ _ => True). ssplit; [ tauto | ].
+  cbn [value circuit_state]; intros. destruct_products.
+  repeat lazymatch goal with x : unit |- _ => destruct x end.
+  ssplit; reflexivity.
 Qed.
-Hint Rewrite @Par_Id_w using solve [eauto] : circuitsimplw.
+Hint Rewrite @Compose_Comb using solve [eauto] : pull_comb.
 
-Lemma Compose_assoc_w A B C D
-      (f : Circuit A B) (g : Circuit B C) (h : Circuit C D) :
-  wequiv (f >==> (g >==> h)) (f >==> g >==> h).
-Admitted.
-Hint Rewrite @Compose_assoc_w using solve [eauto] : circuitsimplw.
-
-Lemma First_Compose_w A B C D (f : Circuit A B) (g : Circuit B C) :
-  @wequiv (A * D) (C * D) (First (f >==> g)) (First f >==> First g).
-Admitted.
-Hint Rewrite @First_Compose_w using solve [eauto] : circuitsimplw.
-
-Lemma Second_Compose_w A B C D (f : Circuit A B) (g : Circuit B C) :
-  @wequiv (D * A) (D * C) (Second (f >==> g)) (Second f >==> Second g).
-Admitted.
-Hint Rewrite @Second_Compose_w using solve [eauto] : circuitsimplw.
-
-Global Instance Proper_Par_w {A B C D} :
-  Proper (wequiv ==> wequiv ==> wequiv) (@Par _ _ A B C D).
+Lemma Compose_Comb_r {i t1 t2 o} c f g :
+  cequiv (@Compose _ _ i t2 o (@Compose _ _ i t1 t2 c (Comb f)) (Comb g))
+         (c >==> Comb (f >=> g)).
 Proof.
-  cbv [Par]. repeat intro.
-  repeat match goal with H : wequiv _ _ |- _ => rewrite H end.
-  reflexivity.
+  rewrite <-Compose_Comb. autorewrite with circuitsimpl. reflexivity.
+Qed.
+Hint Rewrite @Compose_Comb_r using solve [eauto] : pull_comb.
+
+Lemma First_Second_comm {i1 i2 o1 o2} (c1 : Circuit i1 o1) (c2 : Circuit i2 o2) :
+  cequiv (First c1 >==> Second c2) (Second c2 >==> First c1).
+Proof.
+  exists (fun s1 s2 => s1 = (snd s2, fst s2)).
+  ssplit; [ reflexivity | ]. cbn [value circuit_state].
+  intros; destruct_products. cbn [fst snd] in *.
+  logical_simplify. ssplit; reflexivity.
+Qed.
+Hint Rewrite <- @First_Second_comm using solve [eauto] : circuitsimpl.
+
+Lemma Comb_ext {i o} (f g : value i -> cava (value o)) :
+  (forall x, f x = g x) -> cequiv (Comb f) (Comb g).
+Proof.
+  intro Hfg. exists (fun _ _ => True). ssplit; [ tauto | ].
+  cbn [value circuit_state]; intros. destruct_products.
+  repeat lazymatch goal with x : unit |- _ => destruct x end.
+  cbn [step]. rewrite Hfg.
+  ssplit; [ reflexivity | tauto ].
 Qed.
--- cava/Cava/Semantics/WeakEquivalence.v
+++ cava/Cava/Semantics/WeakEquivalence.v
@@ -28,6 +28,7 @@ Require Import Cava.Util.List.
 Require Import Cava.Util.Tactics.
 Import Circuit.Notations.
 
+(*
 (* Circuit equivalence relation that stipulates an equal output after n timesteps *)
 Definition cequivn {i o} (n : nat) (c1 c2 : Circuit i o) : Prop :=
   (* there exists some relation between the circuit states and a counter... *)
@@ -50,49 +51,120 @@ Definition cequivn {i o} (n : nat) (c1 c2 : Circuit i o) : Prop :=
 
 Lemma cequivn_compose {i t o} n m (a b : Circuit i t) (c d : Circuit t o) :
   cequivn n a b -> cequivn m c d -> cequivn (n + m) (a >==> c) (b >==> d).
-Admitted.
-
-(* count the number of delays on the critical path of a circuit *)
-Fixpoint cpath {i o} (c : Circuit i o) : nat :=
-  match c with
-  | Comb _ => 0
-  | First f => cpath f
-  | Second f => cpath f
-  | Compose f g => cpath f + cpath g
-  | DelayInit _=> 1
-  | LoopInitCE _ body => cpath body
-  end.
-
-Definition cequivn_reflexive {i o} (c : Circuit i o) :
-  is_loop_free c = true -> cequivn (cpath c) c c.
 Proof.
-  induction c; cbn [cpath is_loop_free].
-  { exists (fun i s1 s2 => i = 0 /\ s1 = s2).
-    ssplit; cbn [value step circuit_state]; intros; subst.
-    all:logical_simplify.
-    all:ssplit; try reflexivity.
-    all:discriminate. }
-  { rewrite Bool.andb_true_iff; intros; logical_simplify.
-    apply cequivn_compose; auto. }
-  { admit. (* applies across First *) }
-  { admit. (* applies across Second *) }
-  { discriminate. }
-  { intros. exists (fun i s1 s2 => i <= 1 /\ s1 = s2).
-    cbn [value step circuit_state].
-    ssplit; intros; logical_simplify; subst; ssplit;
-      try reflexivity; try lia. }
+  intros [Rab [Hab1 [Hab2 [Hab3 Hab4]]]].
+  intros [Rcd [Hcd1 [Hcd2 [Hcd3 Hcd4]]]].
+  exists (fun i s1 s2 =>
+       (m < i -> Rab (i - m) (fst s1) (fst s2))
+       /\ (i <= m -> Rcd i (snd s1) (snd s2))).
+  cbn [value circuit_state].
+  ssplit; intros; destruct_products; cbn [fst snd step] in *;
+    repeat
+      lazymatch goal with
+      | H : ?x + ?y <= ?y |- _ =>
+        destruct x; [ | lia ]; autorewrite with natsimpl in *
+      | H : ?x < 1 |- _ =>
+        destruct x; [ clear H | lia ]; autorewrite with natsimpl in *
+      | H1 : ?y < ?x -> _, H2 : ?x <= ?y -> _ |- _ =>
+        destr (x <=? y); [ specialize (H2 ltac:(lia)); clear H1
+                         | specialize (H1 ltac:(lia)); clear H2 ]
+      | H : Rab 1 ?s1 ?s2 |- context [step a ?s1] => erewrite Hab3 by eassumption
+      | H1 : ?x <= ?y, H2 : ?y < S ?x |- _ =>
+        assert (x = y) by lia; subst; clear H1
+      | _ => first
+              [ progress (ssplit; intros)
+              | erewrite Hab2 by lia
+              | apply Hcd2; lia
+              | apply Hab4; [ lia | solve [auto] ]
+              | solve [auto]
+              | lia
+              | rewrite Nat.sub_succ_l in * by lia
+              | progress autorewrite with natsimpl in * ]
+      end.
+Qed.
+
+Lemma cequivn_delays {t} (r1 r2 : value t) : cequivn 1 (DelayInit r1) (DelayInit r2).
+Proof.
+  exists (fun i s1 s2 => i <= 1 /\ s1 = s2). cbn [value step circuit_state].
+  ssplit; intros; logical_simplify; subst; ssplit; try reflexivity; lia.
+Qed.
+
+Lemma cequivn_comb {i o} (f g : value i -> cava (value o)) :
+  (forall x, f x = g x) -> cequivn 0 (Comb f) (Comb g).
+Proof.
+  intro Hfg. exists (fun i s1 s2 => i = 0). cbn [value step circuit_state].
+  ssplit; intros; logical_simplify; subst; ssplit;
+    repeat lazymatch goal with x : unit |- _ => destruct x end;
+    rewrite ?Hfg; try reflexivity; lia.
+Qed.
+
+Instance Symmetric_cequivn {i o} n : Symmetric (@cequivn i o n) | 10.
+Proof.
+  intros x y [R ?]. logical_simplify. exists (fun i sx sy => R i sy sx).
+  ssplit; intros; auto; symmetry; auto.
+Qed.
+
+(* Restriction that states a circuit is cequivn-equivalent to itself for some n;
+   that is, the number of delays betweeen the inputs and each of the outputs is
+   the same. This will not include most circuits with loops. *)
+Definition uniformly_timed {i o} n (c : Circuit i o) : Prop := cequivn n c c.
+
+Definition wequiv {i o} (c1 c2 : Circuit i o) : Prop :=
+  (* either both circuits are non-uniformly timed *)
+  ((forall n, ~ uniformly_timed n c1)
+   /\ (forall n, ~ uniformly_timed n c2))
+  (* ...or both are uniformly timed with the same timing, and they are
+     cequivn-equivalent with respect to that timing *)
+  \/ (exists n, uniformly_timed n c1
+          /\ uniformly_timed n c2
+          /\ cequivn n c1 c2).
+
+Instance Reflexive_wequiv {i o} : Reflexive (@wequiv i o) | 10.
+Proof.
+  repeat intro. cbv [wequiv].
+  (* here's where we need to *decide* if c is uniformly timed or not *)
+
+  (* thing that returns value nat i -> value nat o
+     Compose f g := fun is => match time f is with
+                              | Some ns => utime g ns
+
+   *)
+Qed.
+Instance Symmetric_wequiv {i o} : Symmetric (@wequiv i o) | 10.
+Proof.
+  repeat intro. cbv [wequiv] in *. logical_simplify.
+  symmetry; auto.
+Qed.
+Instance Transitive_wequiv {i o} : Transitive (@wequiv i o) | 10.
+Proof.
+  repeat intro. cbv [wequiv] in *. logical_simplify.
+  (* This relation is not transitive; a non-uniform y could make both preconditions vacuous *)
+  symmetry; auto.
+Qed.
+
+Print Circuit.
+
+(* Circuits with uniform timing characteristics; all outputs have the same
+   number of delays between them and the circuit inputs *)
+Inductive UCircuit `{semantics:Cava} : nat -> type -> type -> Type :=
+| Comb : forall {i o : type}, (value i -> cava (value o)) -> UCircuit 0 i o
+| Compose : forall {i t o n m}, UCircuit n i t -> UCircuit m t o -> UCircuit (n + m) i o
+| Par : forall {i1 i2 o1 o2 n}, UCircuit n i1 o1 -> UCircuit n i2 o2 -> UCircuit n (i1 * i2) (o1 * o2)
+| LoopInitCE : forall {i o s n}, @value combType s -> UCircuit n (i * s) (o * s) -> UCircuit 0 (i * Bit) o
+| DelayInitCE
+
+
+
+
+(* Circuit equivalence relation that declares the circuits produce equivalent
+   outputs for equivalent inputs after a certain number of delays *)
+Definition wequiv {i o} (c1 c2 : Circuit i o) : Prop :=
+  exists n, cequivn n c1 c2.
+
+Global Instance Transitive_wequiv {i o : type} : Transitive (@wequiv i o) | 10.
 Admitted.
 
-(* Circuit equivalence relation that declares the circuits have equal critical
-   paths and, if both are loop-free, then they produce equivalent outputs for
-   equivalent inputs (delayed by the critical path number) *)
-Definition wequiv {i o} (c1 c2 : Circuit i o) : Prop :=
-  cpath c1 = cpath c2
-  /\ (is_loop_free c1 = true ->
-     is_loop_free c2 = true ->
-     cequivn (cpath c1) c1 c2).
-
-Global Instance Equivalence_wequiv: forall {i o : type}, Equivalence (@wequiv i o) | 10.
+Global Instance Symmetric_wequiv {i o : type} : Symmetric (@wequiv i o) | 10.
 Admitted.
 
 Global Instance Proper_Second {i o t : type} :
@@ -109,9 +181,9 @@ Admitted.
 
 Lemma wequiv_delays {t} (r1 r2 : value t) : wequiv (DelayInit r1) (DelayInit r2).
 Proof.
-  cbv [wequiv]. cbn [cpath is_loop_free]. ssplit; [ reflexivity | intros ].
-  intros. exists (fun i s1 s2 => i <= 1 /\ s1 = s2).
+  cbv [wequiv]. exists 1. exists (fun i s1 s2 => i <= 1 /\ s1 = s2).
   cbn [value step circuit_state].
   ssplit; intros; logical_simplify; subst; ssplit;
-    try reflexivity; try lia.
+    try reflexivity; lia.
 Qed.
+*)
