433610bcc1eddc69d2dc60b9edb13f3e54139081
Samuel Gruetter <samuelgruetter@google.com>
2021-07-20 17:00:59 -0400

    first sketch of cava_counter_satisfies_state_machine,
    still several problems in the device_state_related relation, but
    already found and fixed two bugs in the Cava IncrementWait circuit:
    - when the result value is read by the software, the device has to
      go back to IDLE
    - the device must leave the IDLE state only if the request is a
      value write request, but stay in IDLE if the request is any other request

--- investigations/bedrock2/RiscvMachineWithCavaDevice/DetIncrMachine.v
+++ investigations/bedrock2/RiscvMachineWithCavaDevice/DetIncrMachine.v
@@ -49,10 +49,16 @@ Section WithCava.
        is_status_req <- Vec.hd req_addr2;;
        is_value_req <- inv is_status_req;;
        is_value_write_req <- and2 (is_value_req, is_write_req);;
+       isnt_value_write_req <- inv is_value_write_req;;
+       is_value_read_req <- and2 (is_value_req, is_read_req);;
        no_pending_inp <- inv is_resp;;
-       idle' <- and2 (idle, no_pending_inp);;
+       result_ready <- or2 (busy, done);;
+       done2idle <- and2 (done, is_value_read_req);;
+       not_done2idle <- inv done2idle;;
+       idle2idle <- and2 (idle, isnt_value_write_req);;
+       idle' <- or2 (idle2idle, done2idle);;
        busy' <- is_value_write_req;;
-       done' <- or2 (busy, done);;
+       done' <- and2 (result_ready, not_done2idle);;
        value_plus_one <- incrN value;;
        value_or_input <- ite is_value_write_req req_value value;;
        value' <- ite busy value_plus_one value_or_input;;
@@ -100,8 +106,9 @@ Example sample_trace := Eval compute in simulate_with_state incr [
 
 Require Import Coq.ZArith.ZArith. Open Scope Z_scope.
 Require Import riscv.Utility.Utility.
-Require Import coqutil.Map.Interface.
-Require Import coqutil.Map.Properties.
+Require Import coqutil.Map.Interface coqutil.Map.Properties.
+Require Import coqutil.Word.Interface coqutil.Word.Properties.
+Require Import coqutil.Tactics.Simp.
 Require Import Bedrock2Experiments.RiscvMachineWithCavaDevice.InternalMMIOMachine.
 Require Import Bedrock2Experiments.IncrementWait.Constants.
 Require Import Bedrock2Experiments.IncrementWait.IncrementWaitSemantics.
@@ -124,8 +131,155 @@ Section WithParameters.
   Global Instance circuit_spec : circuit_behavior :=
   {| ncycles_processing := 15%nat |}.
 
+  Ltac destruct_state s :=
+    destruct s as ([] & (([] & (([] & (([] & ([] & ?val)) & ?done)) & ?busy)) & ?idle)).
+
+  Definition mk_counter_state(idle busy done: bool)(val: Bvector 32): circuit_state incr :=
+    (tt, (tt, (tt, (tt, (tt, val), done), busy), idle)).
+
+  Inductive counter_related: IncrementWaitSemantics.state -> circuit_state incr -> Prop :=
+  | reset_related:
+      counter_related IDLE (reset_state incr)
+  | IDLE_related: forall val,
+      counter_related IDLE (mk_counter_state true false false val)
+  | BUSY_related: forall val ncycles,
+      counter_related (BUSY val ncycles) (mk_counter_state false true false (word_to_bv val))
+  | DONE_related: forall val,
+      counter_related (DONE val) (mk_counter_state false false true (word_to_bv val)).
+
+  Axiom TODO: False.
+
+  Axiom word_to_bv_inj: forall x y, word_to_bv x = word_to_bv y -> x = y.
+
+  Set Printing Depth 100000.
+
   Global Instance cava_counter_satisfies_state_machine:
     device_implements_state_machine counter_device state_machine_parameters.
-  Admitted.
+  Proof.
+    eapply Build_device_implements_state_machine with (device_state_related := counter_related);
+      intros.
+    - (* mmioAddrs_match: *)
+      reflexivity.
+    - (* initial_state_is_reset_state: *)
+      cbn. split; intros.
+      + subst. apply reset_related.
+      + inversion H. reflexivity.
+    - (* state_machine_state_unique: *)
+      inversion H; inversion H0; subst; try reflexivity; try discriminate.
+      + inversion H4.
+        (* Problem: Several high-level states (differning in ncycles) for one low-level state *)
+        case TODO.
+      + inversion H4. apply word_to_bv_inj in H2. subst. reflexivity.
+    - (* nonMMIO_device_step_preserves_state_machine_state: *)
+      simpl in sL1, sL2. destruct_state sL1. destruct_state sL2.
+      cbn [device.run1 counter_device incr_device_step step incr incr_update Loop snd
+           ret bind monad CombinationalSemantics Identity.Monad_ident constant] in H0.
+      eapply pair_equal_spec in H0. destruct H0 as [E1 E2].
+      subst.
+      eapply pair_equal_spec in E1. destruct E1 as [_ E1].
+      eapply pair_equal_spec in E1. destruct E1 as [E1 E2]. simpl in E2. subst.
+      eapply pair_equal_spec in E1. destruct E1 as [_ E1].
+      eapply pair_equal_spec in E1. destruct E1 as [E1 E2]. simpl in E2. subst.
+      eapply pair_equal_spec in E1. destruct E1 as [_ E1].
+      eapply pair_equal_spec in E1. destruct E1 as [E1 E2]. simpl in E2. subst.
+      eapply pair_equal_spec in E1. destruct E1 as [_ E1].
+      eapply pair_equal_spec in E1. destruct E1 as [_ E1]. simpl in E1. subst.
+      inversion H; subst.
+      + (* reset_related: *)
+        cbn. rewrite andb_false_r. cbn.
+        eapply (IDLE_related (Vector.const false 32)).
+      + (* IDLE_related: *)
+        cbn. rewrite andb_false_r. cbn. simpl.
+        assumption.
+      + (* BUSY_related: *)
+        rewrite andb_false_r. cbn -[incrN]. change (Pos.to_nat 1) with 1%nat.
+        cbn -[incrN].
+        (* Problem: device can go from BUSY to DONE without any software interaction *)
+        case TODO.
+      + (* DONE_related: *)
+        cbn. rewrite andb_false_r. cbn. assumption.
+    - (* state_machine_read_to_device_read: *)
+      (* simpler because device.maxRespDelay=1 *)
+      unfold device.maxRespDelay, device.runUntilResp, device.state, device.run1, counter_device.
+      unfold StateMachineSemantics.parameters.read_step, state_machine_parameters in *.
+      destruct H as [? H]. subst.
+      unfold read_step in *.
+      destruct r.
+      + (* r=VALUE *)
+        destruct sH; try contradiction. destruct H. subst.
+        inversion H0; subst.
+        cbn. rewrite andb_false_r. simpl.
+        unfold word_to_bv. rewrite word.unsigned_of_Z_nowrap by (cbv; intuition discriminate).
+        simpl. eexists. split; [reflexivity|]. eapply IDLE_related.
+      + (* r=STATUS *)
+        destruct sH.
+        * (* sH=IDLE *)
+          destruct H. subst. inversion H0.
+          -- (* sL=reset_state *)
+             cbn. rewrite andb_false_r. simpl. unfold status_value, STATUS_IDLE.
+             unfold word_to_bv.
+             rewrite word.unsigned_of_Z_nowrap by (cbv; intuition discriminate).
+             rewrite word.unsigned_slu. 2: rewrite word.unsigned_of_Z. 2: reflexivity.
+             rewrite !word.unsigned_of_Z. unfold word.wrap. cbn.
+             eexists. split; [reflexivity|]. eapply (IDLE_related (Vector.const false 32)).
+          -- (* sL idle *)
+             cbn. rewrite andb_false_r. simpl. unfold status_value, STATUS_IDLE.
+             unfold word_to_bv.
+             rewrite word.unsigned_of_Z_nowrap by (cbv; intuition discriminate).
+             rewrite word.unsigned_slu. 2: rewrite word.unsigned_of_Z. 2: reflexivity.
+             rewrite !word.unsigned_of_Z. unfold word.wrap. cbn.
+             eexists. split; [reflexivity|]. eapply IDLE_related.
+        * (* sH=BUSY *)
+          inversion H0. subst.
+          destruct H as [H | H].
+          -- (* transition to DONE *)
+             destruct H; subst.
+             simpl (StateMachineSemantics.parameters.reg_addr _).
+             unfold STATUS_ADDR, INCR_BASE_ADDR, word_to_bv, status_value, STATUS_DONE.
+             rewrite word.unsigned_slu. 2: rewrite word.unsigned_of_Z. 2: reflexivity.
+             rewrite !word.unsigned_of_Z. unfold word.wrap. cbn -[Vec.hd Vec.tl incrN].
+             rewrite andb_false_r. simpl.
+             eexists.
+             (* Problem: high-level transitions to DONE, but low-level is in BUSY and
+                therefore still answers BUSY, somehow we're shifted by 1 cycle? *)
+             case TODO.
+          -- (* stay BUSY *)
+             destruct H as (n & ? & ? & ?); subst.
+             simpl (StateMachineSemantics.parameters.reg_addr _).
+             unfold STATUS_ADDR, INCR_BASE_ADDR, word_to_bv, status_value, STATUS_BUSY.
+             rewrite word.unsigned_slu. 2: rewrite word.unsigned_of_Z. 2: reflexivity.
+             rewrite !word.unsigned_of_Z. unfold word.wrap. cbn -[Vec.hd Vec.tl incrN].
+             rewrite andb_false_r. simpl.
+             eexists. split; [reflexivity|].
+             (* Problem: value now off by 1 *)
+             case TODO.
+        * (* sH=DONE *)
+          destruct H. subst. inversion H0. subst.
+          simpl (StateMachineSemantics.parameters.reg_addr _).
+          unfold STATUS_ADDR, INCR_BASE_ADDR, word_to_bv, status_value, STATUS_DONE.
+          rewrite word.unsigned_slu. 2: rewrite word.unsigned_of_Z. 2: reflexivity.
+          rewrite !word.unsigned_of_Z. unfold word.wrap. cbn -[Vec.hd Vec.tl incrN].
+          rewrite andb_false_r. simpl.
+          eexists. split; [reflexivity|]. eapply DONE_related.
+    - (* state_machine_write_to_device_write: *)
+      destruct H. subst. unfold write_step in H1.
+      destruct r. 2: contradiction.
+      destruct sH; try contradiction. subst.
+      inversion H0.
+      + (* sL=reset_state *)
+        cbn.
+        unfold word_to_bv.
+        rewrite word.unsigned_of_Z_nowrap by (cbv; intuition discriminate).
+        cbn.
+        eexists _, _. split; [reflexivity|]. eapply BUSY_related.
+      + (* sL idle *)
+        cbn.
+        unfold word_to_bv.
+        rewrite word.unsigned_of_Z_nowrap by (cbv; intuition discriminate).
+        cbn.
+        eexists _, _. split; [reflexivity|]. eapply BUSY_related.
+  Unshelve.
+  all: try exact (reset_state incr).
+  Qed.
 
 End WithParameters.
