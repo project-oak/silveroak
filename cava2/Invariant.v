59e01bb6bf3e9c8e31b70db2f0781f4768f7b7df
Jade Philipoom <jadep@google.com>
2021-09-13 11:04:12 +0100

    changed to use typeclasses

--- cava2/Invariant.v
+++ cava2/Invariant.v
@@ -27,6 +27,38 @@ Require Import Cava.Util.Nat.
 Require Import Cava.Util.Tactics.
 Require Import Cava.ExprProperties.
 
+Class state_logic_for {s i o} (c : Circuit (var:=denote_type) s i o)
+      (hl_state : Type) :=
+  { reset_hl_state : hl_state;
+    update_hl_state : denote_type i -> hl_state -> hl_state;
+    invariant_for : denote_type s -> hl_state -> Prop;
+    precondition_for : denote_type i -> hl_state -> Prop;
+    spec_for : denote_type i -> hl_state -> denote_type o;
+  }.
+Global Arguments invariant_for {_ _ _} _ {_ _}.
+Global Arguments precondition_for {_ _ _} _ {_ _}.
+Global Arguments spec_for {_ _ _} _ {_ _}.
+
+Definition invariant_at_reset {s i o} (c : Circuit s i o)
+           {hl_state} {state_logic : state_logic_for c hl_state} : Prop :=
+  invariant_for c (reset_state c) reset_hl_state.
+
+Definition invariant_preserved {s i o} (c : Circuit s i o)
+           {hl_state} {state_logic : state_logic_for c hl_state} : Prop :=
+  forall input state hl_state new_hl_state,
+    new_hl_state = update_hl_state input hl_state ->
+    invariant_for c state hl_state ->
+    precondition_for c input hl_state ->
+    invariant_for c (fst (step c state input)) new_hl_state.
+
+Definition output_correct {s i o} (c : Circuit s i o)
+           {hl_state} {state_logic : state_logic_for c hl_state} : Prop :=
+  forall input state hl_state,
+    invariant_for c state hl_state ->
+    precondition_for c input hl_state ->
+    snd (step c state input) = spec_for c input hl_state.
+
+
 Module N.
   Lemma mod_mod_mul_l a b c : b <> 0 -> c <> 0 -> (a mod (b * c)) mod c = a mod c.
   Proof.
@@ -107,121 +139,86 @@ Module Example.
   End CircuitDefinitions.
 
   Section Specifications.
-    (* Ghost state : counter value *)
-    Definition counter_hl_state : Type := N.
 
-    (* Invariant: the counter value must be < 2 ^ 8. *)
-    Definition counter_invariant
-               (state : denote_type (state_of (var:=denote_type) counter))
-               (* Ghost variable tracking counter value *)
-               (hl_state : counter_hl_state)
-    : Prop :=
-      let '(data, _) := state in
-      let value := hl_state in
-      (* value is exactly equivalent to [data] *)
-      data = value
-      (* ...and value < 2 ^ 8 *)
-      /\ value < 2 ^ 8.
+    Global Instance counter_state_logic
+      : state_logic_for counter N :=
+      {| reset_hl_state := 0;
+         update_hl_state :=
+           fun (input : denote_type [Bit]) value =>
+             let '(enable,_) := input in
+             (* if enabled, add 1 mod 2^8, else do nothing *)
+             if enable
+             then ((value + 1) mod (2 ^ 8))%N
+             else value;
+         invariant_for :=
+           fun (state : denote_type (BitVec 8 ** _)) value =>
+             let '(data,_) := state in
+             (* value is exactly equivalent to [data] *)
+             data = value
+             (* ...and value < 2 ^ 8 *)
+             /\ value < 2 ^ 8;
+         precondition_for := fun _ _ => True;
+         spec_for :=
+           fun input value =>
+             let '(enable,_) := input in
+             let new_val := if enable then (value + 1)%N else value in
+             ((new_val mod (2 ^ 8), 2 ^ 8 <=? new_val)
+              : denote_type (BitVec 8 ** Bit))
+      |}.
 
-    (* No input preconditions *)
-    Definition counter_pre
-               (input : denote_type (input_of (var:=denote_type) counter))
-               (hl_state : counter_hl_state)
-      : Prop := True.
-
-    Definition counter_spec
-               (input : denote_type (input_of (var:=denote_type) counter))
-               (hl_state : counter_hl_state)
-      : denote_type (output_of (var:=denote_type) counter) :=
-      let '(enable,_) := input in
-      let value := hl_state in
-      let new_val := if enable then (value + 1)%N else value in
-      (new_val mod (2 ^ 8), 2 ^ 8 <=? new_val).
-
-    Definition counter_update_hl_state
-               (input : denote_type (input_of (counter (var:=denote_type))))
-               (hl_state : counter_hl_state) : counter_hl_state :=
-      let '(enable,_) := input in
-      let value := hl_state in
-      if enable
-      then ((value + 1) mod (2 ^ 8))%N
-      else value.
-
-    (* Double counter high-level state is also just a value *)
-    Definition double_counter_hl_state := counter_hl_state.
-
-    Definition double_counter_invariant
-               (state : denote_type (state_of (double_counter (var:=denote_type))))
-               (* High-level state variable tracking double_counter value *)
-               (hl_state : double_counter_hl_state)
-    : Prop :=
-      let '(counter1_state, counter2_state) := state in
-      let value := hl_state in
-      (* counter1_state matches the low part of the counter value *)
-      counter_invariant counter1_state (value mod 2 ^ 8)%N
-      (* ...and counter2_state matches the high part of the counter value *)
-      /\ counter_invariant counter2_state (value / 2 ^ 8)%N
-      (* ...and the value is < 2 ^ 16 (this is implied by the other two but
-         convenient to have without unfolding [counter_invariant]) *)
-      /\ value < 2 ^ 16.
-
-    (* No input preconditions *)
-    Definition double_counter_pre
-               (input : denote_type (input_of (double_counter (var:=denote_type))))
-               (hl_state : double_counter_hl_state)
-      : Prop := True.
-
-    (* Spec is the same as a double-size counter *)
-    Definition double_counter_spec
-               (input : denote_type (input_of (double_counter (var:=denote_type))))
-               (hl_state : double_counter_hl_state)
-      : denote_type (output_of (double_counter (var:=denote_type))) :=
-      let '(enable,_) := input in
-      let value := hl_state in
-      let new_val := if enable then (value + 1)%N else value in
-      (new_val mod (2 ^ 16), 2 ^ 16 <=? new_val).
-
-    Definition double_counter_update_hl_state
-               (input : denote_type (input_of (double_counter (var:=denote_type))))
-               (hl_state : double_counter_hl_state) : double_counter_hl_state :=
-      let '(enable,_) := input in
-      let value := hl_state in
-      if enable
-      then ((value + 1) mod (2 ^ 16))%N
-      else value.
+    Global Instance double_counter_state_logic
+      : state_logic_for double_counter N :=
+      {| reset_hl_state := 0;
+         update_hl_state :=
+           fun (input : denote_type [Bit]) value =>
+             let '(enable,_) := input in
+             (* if enabled, add 1 mod 2^16, else do nothing *)
+             if enable
+             then ((value + 1) mod (2 ^ 16))%N
+             else value;
+         invariant_for :=
+           fun (state : denote_type (state_of (var:=denote_type) counter
+                                            ** state_of (var:=denote_type) counter)) value =>
+             let '(counter1_state, counter2_state) := state in
+             (* counter1_state matches the low part of the counter value *)
+             invariant_for counter counter1_state (value mod 2 ^ 8)%N
+             (* ...and counter2_state matches the high part of the counter value *)
+             /\ invariant_for counter counter2_state (value / 2 ^ 8)%N
+             (* ...and the value is < 2 ^ 16 (this is implied by the other two but
+                convenient to have without unfolding [counter_invariant]) *)
+             /\ value < 2 ^ 16;
+         precondition_for := fun _ _ => True;
+         spec_for :=
+           fun input value =>
+             let '(enable,_) := input in
+             let new_val := if enable then (value + 1)%N else value in
+             ((new_val mod (2 ^ 16), 2 ^ 16 <=? new_val)
+              : denote_type (BitVec 16 ** Bit))
+      |}.
   End Specifications.
 
   Section Proofs.
 
-    Lemma step_counter_invariant state hl_state new_hl_state input :
-      counter_invariant state hl_state ->
-      counter_pre input hl_state ->
-      new_hl_state = counter_update_hl_state input hl_state ->
-      counter_invariant (fst (step counter state input))
-                        new_hl_state.
+    Lemma step_counter_invariant : invariant_preserved counter.
     Proof.
-      destruct state as (data, ?). rename hl_state into value.
-      destruct input as (enable,[]).
-      cbv [counter_invariant counter_pre]; intros; subst.
+      cbv [invariant_preserved]. intros (enable,[]) (data, ?) value.
+      intros; subst.
+      cbn [invariant_for precondition_for update_hl_state
+                         counter_state_logic] in *.
       cbv [counter]. stepsimpl. logical_simplify; subst.
-      cbv [counter_update_hl_state].
       destruct enable; cbn [negb fst snd].
       all:ssplit; first [ lia
                         | reflexivity
                         | apply N.mod_bound_pos; lia ].
     Qed.
 
-    Lemma step_counter state hl_state input :
-      counter_invariant state hl_state ->
-      counter_pre input hl_state ->
-      snd (step counter state input)
-      = counter_spec input hl_state.
+    Lemma step_counter : output_correct counter.
     Proof.
-      destruct state as (data, ?). rename hl_state into value.
-      destruct input as (enable,[]).
-      cbv [counter_invariant counter_pre]; intros; subst.
-      cbv [counter]. stepsimpl. logical_simplify; subst.
-      cbv [counter_spec]. compute_expr (N.of_nat 8).
+      cbv [output_correct]. intros (enable,[]) (data, ?) value.
+      cbn [invariant_for precondition_for spec_for
+                         counter_state_logic] in *.
+      intros; logical_simplify; subst.
+      cbv [counter]. stepsimpl. compute_expr (N.of_nat 8).
       change (2 ^ 8) with 256 in *.
       change (N.ones 8) with 255 in *.
       destruct enable; cbn [negb fst snd].
@@ -233,39 +230,33 @@ Module Example.
       all:rewrite N.mod_small by lia; reflexivity.
     Qed.
 
-    Lemma step_double_counter_invariant state hl_state new_hl_state input :
-      double_counter_invariant state hl_state ->
-      double_counter_pre input hl_state ->
-      new_hl_state = double_counter_update_hl_state input hl_state ->
-      double_counter_invariant (fst (step double_counter state input))
-                               new_hl_state.
+    Lemma step_double_counter_invariant : invariant_preserved double_counter.
     Proof.
-      destruct state as (data, ?). rename hl_state into value.
-      destruct input as (enable,[]).
-      cbv [double_counter_invariant double_counter_pre]; intros; subst.
-      cbv [double_counter]. stepsimpl.
-      logical_simplify; subst. natsimpl.
+      cbv [invariant_preserved]. intros (enable,[]) (data,?) value.
+      intros; subst.
+      cbn [invariant_for precondition_for update_hl_state
+                         double_counter_state_logic] in *.
+      cbv [double_counter]. stepsimpl. logical_simplify; subst.
       lazymatch goal with
       | |- context [match step counter ?s ?i with pair _ _ => _ end] =>
         rewrite (surjective_pairing (step counter s i))
       end.
       erewrite !step_counter by eauto.
-      cbv [counter_spec]. stepsimpl.
+      cbn [spec_for counter_state_logic]. stepsimpl.
       lazymatch goal with
       | |- context [match step counter ?s ?i with pair _ _ => _ end] =>
         rewrite (surjective_pairing (step counter s i))
       end.
       erewrite step_counter by eauto.
-      cbv [counter_spec].
-      stepsimpl.
-      cbv [double_counter_update_hl_state counter_update_hl_state].
+      cbn [spec_for counter_state_logic]. stepsimpl.
       ssplit.
-      { eapply step_counter_invariant; [ solve [eauto] .. | ].
-        cbv [counter_update_hl_state]. destruct enable; [ | reflexivity ].
+      { eapply step_counter_invariant; [ | solve [eauto] .. ].
+        cbn [update_hl_state counter_state_logic].
+        destruct enable; [ | reflexivity ].
         change (2 ^ 8) with 256 in *. change (2 ^ 16) with 65536 in *.
         Zify.zify. Z.to_euclidean_division_equations. lia. }
-      { eapply step_counter_invariant; [ solve [eauto] .. | ].
-        cbv [counter_update_hl_state].
+      { eapply step_counter_invariant; [ | solve [eauto] .. ].
+        cbn [update_hl_state counter_state_logic].
         change (2 ^ 8) with 256 in *. change (2 ^ 16) with 65536 in *.
         repeat (destruct_one_match; try lia).
         (* extra step to help lia out, otherwise hangs *)
@@ -275,31 +266,25 @@ Module Example.
         apply N.mod_bound_pos; lia. }
     Qed.
 
-    Lemma step_double_counter state hl_state input :
-      double_counter_invariant state hl_state ->
-      double_counter_pre input hl_state ->
-      snd (step double_counter state input)
-      = double_counter_spec input hl_state.
+    Lemma step_double_counter : output_correct double_counter.
     Proof.
-      destruct state as (data, ?). rename hl_state into value.
-      destruct input as (enable,[]).
-      cbv [double_counter_invariant double_counter_pre]; intros; subst.
-      cbv [double_counter double_counter_spec]. stepsimpl.
-      logical_simplify; subst. natsimpl.
+      cbv [output_correct]. intros (enable,[]) (data, ?) value.
+      cbn [invariant_for precondition_for spec_for
+                         double_counter_state_logic] in *.
+      intros; logical_simplify; subst.
+      cbv [double_counter]. stepsimpl.
       lazymatch goal with
       | |- context [match step counter ?s ?i with pair _ _ => _ end] =>
         rewrite (surjective_pairing (step counter s i))
       end.
       erewrite step_counter by eauto.
-      cbv [counter_spec].
-      stepsimpl.
+      cbn [spec_for counter_state_logic]; stepsimpl.
       lazymatch goal with
       | |- context [match step counter ?s ?i with pair _ _ => _ end] =>
         rewrite (surjective_pairing (step counter s i))
       end.
       erewrite step_counter by eauto.
-      cbv [counter_spec].
-      stepsimpl.
+      cbn [spec_for counter_state_logic]; stepsimpl.
       f_equal.
       { (* counter values match *)
         compute_expr (N.of_nat 8). compute_expr (8 + 8)%nat.
