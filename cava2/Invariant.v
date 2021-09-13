233475389c793399e71e71c39b569d73cd139efa
Jade Philipoom <jadep@google.com>
2021-09-13 08:36:54 +0100

    WIP

--- cava2/Invariant.v
+++ cava2/Invariant.v
@@ -15,6 +15,7 @@
 (****************************************************************************)
 
 Require Import Coq.NArith.NArith.
+Require Import Coq.ZArith.ZArith.
 Require Import Cava.Types.
 Require Import Cava.Expr.
 Require Import Cava.Semantics.
@@ -57,107 +58,114 @@ Module Example.
     Import PrimitiveNotations.
 
     (* Circuit which takes a bit indicating whether to increment or not, and if
-       the bit is true increments an n-bit counter by 1 each cycle. The counter
+       the bit is true increments an 8-bit counter by 1 each cycle. The counter
        truncates on overflow and returns the counter value along with a bit
        indicating whether the counter overflowed. *)
-    Definition counter (n : nat) : Circuit (BitVec n ** Bit) [Bit] (BitVec n ** Bit) :=
+    Definition counter : Circuit (BitVec 8 ** Bit) [Bit] (BitVec 8 ** Bit) :=
       {{ fun enable =>
            let/delay '(data; overflow) :=
               (if !enable
                then (data,`Zero`)
                else
-                 let new_overflow := data == `K (N.ones (N.of_nat n))` in
+                 let new_overflow := data == `K (N.ones 8)` in
                  let new_data := data + `K 1` in
                  (new_data, new_overflow))
                 initially
-                ((0,false) : denote_type (BitVec n ** Bit)) in
+                ((0,false) : denote_type (BitVec 8 ** Bit)) in
            (data,overflow)
       }}.
 
-    (* Creates a 2n-bit counter out of two n-bit counters *)
-    Definition double_counter (n : nat) : Circuit _ [Bit] (BitVec (2 * n) ** Bit) :=
+    (* Creates a 16-bit counter out of two 8-bit counters *)
+    Definition double_counter : Circuit _ [Bit] (BitVec 16 ** Bit) :=
       {{ fun enable =>
-           let '(low; low_overflow) := `counter n` enable in
-           let '(high; high_overflow) := `counter n` low_overflow in
+           let '(low; low_overflow) := `counter` enable in
+           let '(high; high_overflow) := `counter` low_overflow in
            (`bvresize _` (`bvconcat` high low), high_overflow) }}.
   End CircuitDefinitions.
 
   Section Specifications.
     (* Ghost state : counter value *)
-    Definition counter_ghost_state : Type := N.
+    Definition counter_hl_state : Type := N.
 
-    (* Invariant: the counter value must be < 2 ^ n. *)
-    Definition counter_invariant {n}
-               (state : denote_type (state_of (var:=denote_type) (counter n)))
+    (* Invariant: the counter value must be < 2 ^ 8. *)
+    Definition counter_invariant
+               (state : denote_type (state_of (var:=denote_type) counter))
                (* Ghost variable tracking counter value *)
-               (ghost_state : counter_ghost_state)
+               (hl_state : counter_hl_state)
     : Prop :=
       let '(data, _) := state in
-      let value := ghost_state in
+      let value := hl_state in
       (* value is exactly equivalent to [data] *)
       data = value
-      (* ...and value < 2 ^ n *)
-      /\ (value < 2 ^ N.of_nat n)%N.
+      (* ...and value < 2 ^ 8 *)
+      /\ value < 2 ^ 8.
 
     (* No input preconditions *)
-    Definition counter_pre {n}
-               (input : denote_type (input_of (var:=denote_type) (counter n)))
-               (ghost_state : counter_ghost_state)
+    Definition counter_pre
+               (input : denote_type (input_of (var:=denote_type) counter))
+               (hl_state : counter_hl_state)
       : Prop := True.
 
-    Definition counter_spec {n}
-               (input : denote_type (input_of (var:=denote_type) (counter n)))
-               (ghost_state : counter_ghost_state)
-      : denote_type (output_of (var:=denote_type) (counter n)) :=
+    Definition counter_spec
+               (input : denote_type (input_of (var:=denote_type) counter))
+               (hl_state : counter_hl_state)
+      : denote_type (output_of (var:=denote_type) counter) :=
       let '(enable,_) := input in
-      let value := ghost_state in
+      let value := hl_state in
       let new_val := if enable then (value + 1)%N else value in
-      (new_val mod (2 ^ N.of_nat n), (2 ^ N.of_nat n <=? new_val))%N.
+      (new_val mod (2 ^ 8), 2 ^ 8 <=? new_val).
 
-    Definition counter_update_ghost_state {n}
-               (input : denote_type (input_of (counter (var:=denote_type) n)))
-               (ghost_state : counter_ghost_state) : counter_ghost_state :=
+    Definition counter_update_hl_state
+               (input : denote_type (input_of (counter (var:=denote_type))))
+               (hl_state : counter_hl_state) : counter_hl_state :=
       let '(enable,_) := input in
-      let value := ghost_state in
+      let value := hl_state in
       if enable
-      then ((value + 1) mod (2 ^ N.of_nat n))%N
+      then ((value + 1) mod (2 ^ 8))%N
       else value.
 
-    (* Double counter ghost state is also just a value *)
-    Definition double_counter_ghost_state := counter_ghost_state.
+    (* Double counter high-level state is also just a value *)
+    Definition double_counter_hl_state := counter_hl_state.
 
-    Definition double_counter_invariant {n}
-               (state : denote_type (state_of (double_counter (var:=denote_type) n)))
-               (* Ghost variable tracking double_counter value *)
-               (ghost_state : double_counter_ghost_state)
+    Definition double_counter_invariant
+               (state : denote_type (state_of (double_counter (var:=denote_type))))
+               (* High-level state variable tracking double_counter value *)
+               (hl_state : double_counter_hl_state)
     : Prop :=
       let '(counter1_state, counter2_state) := state in
-      let value := ghost_state in
+      let value := hl_state in
       (* counter1_state matches the low part of the counter value *)
-      counter_invariant (n:=n) counter1_state (value mod 2 ^ N.of_nat n)%N
+      counter_invariant counter1_state (value mod 2 ^ 8)%N
       (* ...and counter2_state matches the high part of the counter value *)
-      /\ counter_invariant (n:=n) counter2_state (value / 2 ^ N.of_nat n)%N
-      (* ...and the value is < 2 ^ 2n (this is implied by the other two but
+      /\ counter_invariant counter2_state (value / 2 ^ 8)%N
+      (* ...and the value is < 2 ^ 16 (this is implied by the other two but
          convenient to have without unfolding [counter_invariant]) *)
-      /\ value < 2 ^ (N.of_nat (2 * n)).
+      /\ value < 2 ^ 16.
 
     (* No input preconditions *)
-    Definition double_counter_pre {n}
-               (input : denote_type (input_of (double_counter (var:=denote_type) n)))
-               (ghost_state : double_counter_ghost_state)
+    Definition double_counter_pre
+               (input : denote_type (input_of (double_counter (var:=denote_type))))
+               (hl_state : double_counter_hl_state)
       : Prop := True.
 
     (* Spec is the same as a double-size counter *)
-    Definition double_counter_spec {n}
-               (input : denote_type (input_of (double_counter (var:=denote_type) n)))
-               (ghost_state : double_counter_ghost_state)
-      : denote_type (output_of (double_counter (var:=denote_type) n)) :=
-      counter_spec (n:=2*n) input ghost_state.
+    Definition double_counter_spec
+               (input : denote_type (input_of (double_counter (var:=denote_type))))
+               (hl_state : double_counter_hl_state)
+      : denote_type (output_of (double_counter (var:=denote_type))) :=
+      let '(enable,_) := input in
+      let value := hl_state in
+      let new_val := if enable then (value + 1)%N else value in
+      (new_val mod (2 ^ 16), 2 ^ 16 <=? new_val).
 
-    Definition double_counter_update_ghost_state {n}
-               (input : denote_type (input_of (double_counter (var:=denote_type) n)))
-               (ghost_state : double_counter_ghost_state) : double_counter_ghost_state :=
-      counter_update_ghost_state (n:=2*n) input ghost_state.
+    Definition double_counter_update_hl_state
+               (input : denote_type (input_of (double_counter (var:=denote_type))))
+               (hl_state : double_counter_hl_state) : double_counter_hl_state :=
+      let '(enable,_) := input in
+      let value := hl_state in
+      if enable
+      then ((value + 1) mod (2 ^ 16))%N
+      else value.
   End Specifications.
 
   Section Proofs.
@@ -182,54 +190,83 @@ Module Example.
     Qed.
     Hint Rewrite @step_bvconcat using solve [eauto] : stepsimpl.
 
-    Lemma step_counter_invariant n state ghost_state new_ghost_state input :
-      counter_invariant (n:=n) state ghost_state ->
-      counter_pre (n:=n) input ghost_state ->
-      new_ghost_state = counter_update_ghost_state input ghost_state ->
-      counter_invariant (fst (step (counter n) state input))
-                        new_ghost_state.
+    Lemma step_counter_invariant state hl_state new_hl_state input :
+      counter_invariant state hl_state ->
+      counter_pre input hl_state ->
+      new_hl_state = counter_update_hl_state input hl_state ->
+      counter_invariant (fst (step counter state input))
+                        new_hl_state.
     Admitted.
 
-    Lemma step_counter n state ghost_state input :
-      counter_invariant (n:=n) state ghost_state ->
-      counter_pre (n:=n) input ghost_state ->
-      snd (step (counter n) state input)
-      = counter_spec input ghost_state.
+    Lemma step_counter state hl_state input :
+      counter_invariant state hl_state ->
+      counter_pre input hl_state ->
+      snd (step counter state input)
+      = counter_spec input hl_state.
     Admitted.
 
-    Lemma step_double_counter_invariant n state ghost_state new_ghost_state input :
-      double_counter_invariant (n:=n) state ghost_state ->
-      double_counter_pre (n:=n) input ghost_state ->
-      new_ghost_state = double_counter_update_ghost_state input ghost_state ->
-      double_counter_invariant (fst (step (double_counter n) state input))
-                               new_ghost_state.
+    Lemma step_double_counter_invariant state hl_state new_hl_state input :
+      double_counter_invariant state hl_state ->
+      double_counter_pre input hl_state ->
+      new_hl_state = double_counter_update_hl_state input hl_state ->
+      double_counter_invariant (fst (step double_counter state input))
+                               new_hl_state.
     Proof.
-      destruct state as (data, ?). rename ghost_state into value.
+      destruct state as (data, ?). rename hl_state into value.
       destruct input as (enable,[]).
       cbv [double_counter_invariant double_counter_pre]; intros; subst.
       cbv [double_counter]. stepsimpl.
       logical_simplify; subst. natsimpl.
       lazymatch goal with
-      | |- context [match step (counter ?n) ?s ?i with pair _ _ => _ end] =>
-        rewrite (surjective_pairing (step (counter n) s i))
+      | |- context [match step counter ?s ?i with pair _ _ => _ end] =>
+        rewrite (surjective_pairing (step counter s i))
       end.
       erewrite step_counter by eauto.
       cbv [counter_spec].
       stepsimpl.
       lazymatch goal with
-      | |- context [match step (counter ?n) ?s ?i with pair _ _ => _ end] =>
-        rewrite (surjective_pairing (step (counter n) s i))
+      | |- context [match step counter ?s ?i with pair _ _ => _ end] =>
+        rewrite (surjective_pairing (step counter s i))
       end.
       erewrite step_counter by eauto.
       cbv [counter_spec].
       stepsimpl.
+      cbv [double_counter_update_hl_state counter_update_hl_state].
+      ssplit.
+      { eapply step_counter_invariant; [ solve [eauto] .. | ].
+        cbv [counter_update_hl_state]. destruct enable; [ | reflexivity ].
+        change (2 ^ 8) with 256 in *. change (2 ^ 16) with 65536 in *.
+        Zify.zify. Z.to_euclidean_division_equations. lia. }
+      { eapply step_counter_invariant; [ solve [eauto] .. | ].
+        cbv [counter_update_hl_state].
+        change (2 ^ 8) with 256 in *. change (2 ^ 16) with 65536 in *.
+        repeat (destruct_one_match; try lia).
+        { Zify.zify;
+          Z.to_euclidean_division_equations;
+          lia. }
+        { Zify.zify.
+          Z.to_euclidean_division_equations.
+          lia. }
+        { rewrite N.div_small by (apply N.mod_bound_pos; lia).
+          assert (value / 256 < 256) by admit.
+          Zify.zify.
+          Search Z.rem Z.modulo.
+          rewrite !Z.rem_mod_nonneg in * by lia.
+          Z.div_mod_to_equations.
+          lia.
+          Z.to_euclidean_division_equations. lia. }
+        
+        all:Zify.zify; Z.to_euclidean_division_equations; try lia. }
+
+
+      
       (* helpful assertions *)
       assert (2 ^ N.of_nat n <> 0) by (apply N.pow_nonzero; lia).
       pose proof N.mod_bound_pos value (2 ^ N.of_nat n) ltac:(lia) ltac:(lia).
-      cbv [double_counter_update_ghost_state counter_update_ghost_state].
+      cbv [double_counter_update_hl_state counter_update_hl_state].
       ssplit.
       { eapply step_counter_invariant; [ solve [eauto] .. | ].
-        cbv [counter_update_ghost_state].
+        cbv [counter_update_hl_state].
         destruct enable; [ | reflexivity ].
         rewrite N.add_mod_idemp_l, Nat2N.inj_mul by lia.
         replace (N.of_nat 2 * N.of_nat n) with (N.of_nat n * 2) by lia.
@@ -237,7 +274,7 @@ Module Example.
         rewrite N.mod_mod_mul_l by lia.
         reflexivity. }
       { eapply step_counter_invariant; [ solve [eauto] .. | ].
-        cbv [counter_update_ghost_state].
+        cbv [counter_update_hl_state].
         repeat (destruct_one_match; try lia).
         { (* this is the case in which the low counter overflows -- adding 1 and
              then taking the high part is therefore the same as taking the high
@@ -274,13 +311,13 @@ Module Example.
         apply N.mod_bound_pos; lia. }
     Qed.
 
-    Lemma step_double_counter n state ghost_state input :
-      double_counter_invariant (n:=n) state ghost_state ->
-      double_counter_pre (n:=n) input ghost_state ->
+    Lemma step_double_counter n state hl_state input :
+      double_counter_invariant state hl_state ->
+      double_counter_pre input hl_state ->
       snd (step (double_counter n) state input)
-      = double_counter_spec input ghost_state.
+      = double_counter_spec input hl_state.
     Proof.
-      destruct state as (data, ?). rename ghost_state into value.
+      destruct state as (data, ?). rename hl_state into value.
       destruct input as (enable,[]).
       cbv [double_counter_invariant double_counter_pre]; intros; subst.
       cbv [double_counter double_counter_spec]. stepsimpl.
